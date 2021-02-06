931c6a7d43a489c86c63794539429ce33eb059ef
Ailitonia <ailitonia@gmail.com>
2021-02-06 19:26:33 +0800

    add mita button
    + 新增miya按钮（测试

---
+++ omega_miya/plugins/miya_button/__init__.py
@@ -0,0 +1,35 @@
+import re
+import os
+from nonebot import MatcherGroup, logger
+from nonebot.typing import T_State
+from nonebot.rule import to_me
+from nonebot.adapters.cqhttp.bot import Bot
+from nonebot.adapters.cqhttp.message import MessageSegment
+from nonebot.adapters.cqhttp.event import GroupMessageEvent
+from nonebot.adapters.cqhttp.permission import GROUP
+from omega_miya.utils.Omega_plugin_utils import has_command_permission, permission_level
+from .resources import MiyaVoice
+
+"""
+miya按钮bot实现版本
+测试中
+"""
+
+
+button = MatcherGroup(type='message', rule=to_me() & has_command_permission() & permission_level(level=10),
+                      permission=GROUP, priority=100, block=False)
+
+
+miya_button = button.on_endswith(msg='喵一个')
+
+
+@miya_button.handle()
+async def miya_button(bot: Bot, event: GroupMessageEvent, state: T_State):
+    arg = str(event.get_plaintext()).strip().lower()
+    voice = re.sub('喵一个', '', arg)
+    voice_file = MiyaVoice().get_voice_filepath(voice=voice)
+    if not os.path.exists(voice_file):
+        await bot.send(event=event, message='喵？')
+    else:
+        msg = MessageSegment.record(file=f'file:///{voice_file}')
+        await bot.send(event=event, message=msg)
---
+++ omega_miya/plugins/miya_button/resources/__init__.py
@@ -0,0 +1,60 @@
+import os
+import random
+
+
+class Voice(object):
+    def __init__(self):
+        self.VoicesFiles = None
+
+    def get_voice_filepath(self, voice: str) -> str:
+        plugin_path = os.path.dirname(os.path.abspath(__file__))
+        if voice in self.VoicesFiles.keys():
+            return os.path.join(plugin_path, 'voices', self.VoicesFiles[voice]['file'])
+        else:
+            voice_list = []
+            for name, content in self.VoicesFiles.items():
+                if voice == content['tag']:
+                    voice_list.append(content['file'])
+            if not voice_list:
+                return ''
+            else:
+                return os.path.join(plugin_path, 'voices', random.choice(voice_list))
+
+
+class MiyaVoice(Voice):
+    def __init__(self):
+        """
+        硬编码音频素材文件
+        先暂时这样以后有空再改
+        """
+        super().__init__()
+        self.VoicesFiles = {
+            '表演绝活': {
+                'file': '0.mp3',
+                'tag': ''
+            },
+            '圣经': {
+                'file': '1.mp3',
+                'tag': ''
+            },
+            '你才千岁幼猫': {
+                'file': '2.mp3',
+                'tag': ''
+            },
+            '坏蛋': {
+                'file': '3.mp3',
+                'tag': ''
+            },
+            '我信了你的鬼话': {
+                'file': '4.mp3',
+                'tag': ''
+            },
+            '来打我': {
+                'file': '5.mp3',
+                'tag': ''
+            },
+            '说别人憨的人': {
+                'file': '6.mp3',
+                'tag': ''
+            }
+        }
