91bdda50b4ae16fd52b7b3d024fd45e0335e71c3
Ailitonia <ailitonia@gmail.com>
2021-04-10 16:36:22 +0800

    sqla异步重构-subscription
    sqla异步重构-subscription

--- omega_miya/utils/Omega_Base/model/subscription.py
+++ omega_miya/utils/Omega_Base/model/subscription.py
@@ -1,6 +1,7 @@
 from omega_miya.utils.Omega_Base.database import NBdb, DBResult
 from omega_miya.utils.Omega_Base.tables import Subscription, Group, GroupSub
 from datetime import datetime
+from sqlalchemy.future import select
 from sqlalchemy.orm.exc import NoResultFound, MultipleResultsFound
 
 
@@ -9,105 +10,129 @@ class DBSubscription(object):
         self.sub_type = sub_type
         self.sub_id = sub_id
 
-    def id(self) -> DBResult:
-        session = NBdb().get_session()
-        try:
-            subscription_table_id = session.query(Subscription.id).filter(Subscription.sub_type == self.sub_type).\
-                filter(Subscription.sub_id == self.sub_id).one()[0]
-            result = DBResult(error=False, info='Success', result=subscription_table_id)
-        except NoResultFound:
-            result = DBResult(error=True, info='NoResultFound', result=-1)
-        except MultipleResultsFound:
-            result = DBResult(error=True, info='MultipleResultsFound', result=-1)
-        except Exception as e:
-            result = DBResult(error=True, info=repr(e), result=-1)
-        finally:
-            session.close()
+    async def id(self) -> DBResult:
+        async_session = NBdb().get_async_session()
+        async with async_session() as session:
+            async with session.begin():
+                try:
+                    session_result = await session.execute(
+                        select(Subscription.id).
+                        where(Subscription.sub_type == self.sub_type).
+                        where(Subscription.sub_id == self.sub_id)
+                    )
+                    subscription_table_id = session_result.scalar_one()
+                    result = DBResult(error=False, info='Success', result=subscription_table_id)
+                except NoResultFound:
+                    result = DBResult(error=True, info='NoResultFound', result=-1)
+                except MultipleResultsFound:
+                    result = DBResult(error=True, info='MultipleResultsFound', result=-1)
+                except Exception as e:
+                    result = DBResult(error=True, info=repr(e), result=-1)
         return result
 
-    def exist(self) -> bool:
-        result = self.id().success()
-        return result
+    async def exist(self) -> bool:
+        result = await self.id()
+        return result.success()
 
-    def add(self, up_name: str, live_info: str = None) -> DBResult:
-        session = NBdb().get_session()
-        try:
-            # 已存在则更新描述
-            exist_subscription = session.query(Subscription).filter(Subscription.sub_type == self.sub_type).\
-                filter(Subscription.sub_id == self.sub_id).one()
-            exist_subscription.up_name = up_name
-            exist_subscription.live_info = live_info
-            exist_subscription.updated_at = datetime.now()
-            session.commit()
-            result = DBResult(error=False, info='Success upgraded', result=0)
-        except NoResultFound:
-            # 不存在则添加新订阅信息
+    async def add(self, up_name: str, live_info: str = None) -> DBResult:
+        async_session = NBdb().get_async_session()
+        async with async_session() as session:
             try:
-                new_subscription = Subscription(sub_type=self.sub_type, sub_id=self.sub_id,
-                                                up_name=up_name, live_info=live_info, created_at=datetime.now())
-                session.add(new_subscription)
-                session.commit()
-                result = DBResult(error=False, info='Success added', result=0)
+                async with session.begin():
+                    try:
+                        session_result = await session.execute(
+                            select(Subscription).
+                            where(Subscription.sub_type == self.sub_type).
+                            where(Subscription.sub_id == self.sub_id)
+                        )
+                        exist_subscription = session_result.scalar_one()
+                        exist_subscription.up_name = up_name
+                        exist_subscription.live_info = live_info
+                        exist_subscription.updated_at = datetime.now()
+                        result = DBResult(error=False, info='Success upgraded', result=0)
+                    except NoResultFound:
+                        new_subscription = Subscription(sub_type=self.sub_type, sub_id=self.sub_id,
+                                                        up_name=up_name, live_info=live_info, created_at=datetime.now())
+                        session.add(new_subscription)
+                        result = DBResult(error=False, info='Success added', result=0)
+                await session.commit()
+            except MultipleResultsFound:
+                await session.rollback()
+                result = DBResult(error=True, info='MultipleResultsFound', result=-1)
             except Exception as e:
-                session.rollback()
+                await session.rollback()
                 result = DBResult(error=True, info=repr(e), result=-1)
-        except MultipleResultsFound:
-            result = DBResult(error=True, info='MultipleResultsFound', result=-1)
-        except Exception as e:
-            session.rollback()
-            result = DBResult(error=True, info=repr(e), result=-1)
-        finally:
-            session.close()
         return result
 
-    def delete(self) -> DBResult:
-        session = NBdb().get_session()
-        try:
-            # 清空持已订阅这个sub的群组
-            self.sub_group_clear()
-            exist_subscription = session.query(Subscription).filter(Subscription.sub_type == self.sub_type).\
-                filter(Subscription.sub_id == self.sub_id).one()
-            session.delete(exist_subscription)
-            session.commit()
-            result = DBResult(error=False, info='Success', result=0)
-        except NoResultFound:
-            result = DBResult(error=True, info='NoResultFound', result=-1)
-        except MultipleResultsFound:
-            result = DBResult(error=True, info='MultipleResultsFound', result=-1)
-        except Exception as e:
-            session.rollback()
-            result = DBResult(error=True, info=repr(e), result=-1)
-        finally:
-            session.close()
-        return result
+    async def delete(self) -> DBResult:
+        id_result = await self.id()
+        if id_result.error:
+            return DBResult(error=True, info='Subscription not exist', result=-1)
 
-    def sub_group_list(self) -> DBResult:
-        session = NBdb().get_session()
-        res = []
-        if self.exist():
-            for item in session.query(Group.group_id).join(GroupSub). \
-                    filter(Group.id == GroupSub.group_id). \
-                    filter(GroupSub.sub_id == self.id().result).all():
-                res.append(item[0])
-            result = DBResult(error=False, info='Success', result=res)
-        else:
-            result = DBResult(error=True, info='Subscription not exist', result=res)
-        session.close()
-        return result
+        # 清空持已订阅这个sub的群组
+        await self.sub_group_clear()
 
-    def sub_group_clear(self) -> DBResult:
-        if self.exist():
-            session = NBdb().get_session()
+        async_session = NBdb().get_async_session()
+        async with async_session() as session:
             try:
-                for exist_group_sub in session.query(GroupSub).filter(GroupSub.sub_id == self.id().result).all():
-                    session.delete(exist_group_sub)
-                session.commit()
+                async with session.begin():
+                    session_result = await session.execute(
+                        select(Subscription).
+                        where(Subscription.sub_type == self.sub_type).
+                        where(Subscription.sub_id == self.sub_id)
+                    )
+                    exist_subscription = session_result.scalar_one()
+                    await session.delete(exist_subscription)
+                await session.commit()
+                result = DBResult(error=False, info='Success', result=0)
+            except NoResultFound:
+                await session.rollback()
+                result = DBResult(error=True, info='NoResultFound', result=-1)
+            except MultipleResultsFound:
+                await session.rollback()
+                result = DBResult(error=True, info='MultipleResultsFound', result=-1)
+            except Exception as e:
+                await session.rollback()
+                result = DBResult(error=True, info=repr(e), result=-1)
+        return result
+
+    async def sub_group_list(self) -> DBResult:
+        id_result = await self.id()
+        if id_result.error:
+            return DBResult(error=True, info='Subscription not exist', result=[])
+
+        async_session = NBdb().get_async_session()
+        async with async_session() as session:
+            async with session.begin():
+                try:
+                    session_result = await session.execute(
+                        select(Group.group_id).join(GroupSub).
+                        where(Group.id == GroupSub.group_id).
+                        where(GroupSub.sub_id == id_result.result)
+                    )
+                    res = [x for x in session_result.scalars().all()]
+                    result = DBResult(error=False, info='Success', result=res)
+                except Exception as e:
+                    result = DBResult(error=True, info=repr(e), result=[])
+        return result
+
+    async def sub_group_clear(self) -> DBResult:
+        id_result = await self.id()
+        if id_result.error:
+            return DBResult(error=True, info='Subscription not exist', result=-1)
+
+        async_session = NBdb().get_async_session()
+        async with async_session() as session:
+            try:
+                async with session.begin():
+                    session_result = await session.execute(
+                        select(GroupSub).where(GroupSub.sub_id == id_result.result)
+                    )
+                    for exist_group_sub in session_result.scalars().all():
+                        await session.delete(exist_group_sub)
+                await session.commit()
                 result = DBResult(error=False, info='Success', result=0)
             except Exception as e:
-                session.rollback()
+                await session.rollback()
                 result = DBResult(error=True, info=repr(e), result=-1)
-            finally:
-                session.close()
-        else:
-            result = DBResult(error=True, info='Subscription not exist', result=-1)
         return result
