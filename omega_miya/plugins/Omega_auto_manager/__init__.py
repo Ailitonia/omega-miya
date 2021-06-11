2b7f33271a74ed20b21f3f03582d3fc764394f51
Ailitonia <ailitonia@gmail.com>
2021-06-12 00:56:20 +0800

    Upgrade: omega_auto_manager重构, 更新群新增成员欢迎信息部分(#35)
    - omega_auto_manager重构, 更新群新增成员欢迎信息部分

---
+++ omega_miya/plugins/Omega_auto_manager/__init__.py
@@ -0,0 +1,39 @@
+"""
+@Author         : Ailitonia
+@Date           : 2021/06/09 19:10
+@FileName       : __init__.py.py
+@Project        : nonebot2_miya 
+@Description    : Omega 自动化综合/群管/好友管理插件
+@GitHub         : https://github.com/Ailitonia
+@Software       : PyCharm 
+"""
+
+from nonebot import export
+from omega_miya.utils.Omega_plugin_utils import init_export
+from .group_welcome_message import *
+from .invite_request import *
+
+
+# Custom plugin usage text
+__plugin_name__ = 'OmegaAutoManager'
+__plugin_usage__ = r'''【Omega 自动化综合/群管/好友管理插件】
+Omega机器人自动化综合/群管/好友管理
+
+以下命令均需要@机器人
+**Usage**
+**GroupAdmin and SuperUser Only**
+[设置和重置群组加群欢迎信息]
+/WelcomeMsg.set
+/WelcomeMsg.clear
+'''
+
+# Init plugin export
+init_export(export(), __plugin_name__, __plugin_usage__)
+
+
+__all__ = [
+    'welcome_msg_set',
+    'welcome_msg_clear',
+    'group_increase',
+    'add_and_invite_request'
+]
--- omega_miya/plugins/Omega_auto_manager/invite_request.py
+++ omega_miya/plugins/Omega_auto_manager/invite_request.py
@@ -1,3 +1,13 @@
+"""
+@Author         : Ailitonia
+@Date           : 2021/06/12 0:28
+@FileName       : invite_request.py
+@Project        : nonebot2_miya 
+@Description    : 
+@GitHub         : https://github.com/Ailitonia
+@Software       : PyCharm 
+"""
+
 from nonebot import on_request, on_notice, logger
 from nonebot.typing import T_State
 from nonebot.adapters.cqhttp.bot import Bot
@@ -39,20 +49,6 @@ async def handle_group_invite(bot: Bot, event: GroupRequestEvent, state: T_State
         logger.info(f'已处理群组请求, 被用户: {user_id} 邀请加入群组: {group_id}.')
 
 
-# 注册事件响应器, 新增群成员
-group_increase = on_notice(priority=100)
-
-
-@group_increase.handle()
-async def handle_group_increase(bot: Bot, event: GroupIncreaseNoticeEvent, state: T_State):
-    user_id = event.user_id
-    group_id = event.group_id
-    detail_type = event.notice_type
-    self_bot = DBBot(self_qq=int(bot.self_id))
-    group_c_permission_res = await DBBotGroup(group_id=group_id, self_bot=self_bot).permission_command()
-    if detail_type == 'group_increase' and group_c_permission_res.result == 1:
-        # 发送欢迎消息
-        at_seg = MessageSegment.at(user_id=user_id)
-        msg = f'{at_seg}欢迎新朋友～\n进群请先看群公告～\n一起愉快地聊天吧!'
-        await bot.send(event=event, message=Message(msg))
-        logger.info(f'群组: {group_id}, 有新用户: {user_id} 进群')
+__all__ = [
+    'add_and_invite_request'
+]
