8d0f3587661eb2977585b4d9fecac4b4c84b7c9b
Ailitonia <ailitonia@gmail.com>
2021-06-27 04:03:39 +0800

    Upgrade: 优化roll插件掷骰表达式解析
    - 优化roll插件掷骰表达式解析

--- omega_miya/plugins/roll/__init__.py
+++ omega_miya/plugins/roll/__init__.py
@@ -79,20 +79,25 @@ async def handle_roll(bot: Bot, event: GroupMessageEvent, state: T_State):
         dice_info = _roll.split('d')
         dice_num = int(dice_info[0])
         dice_side = int(dice_info[1])
-        # 加入一个趣味的机制
-        if random.randint(1, 100) == 99:
-            await roll.finish(f'【彩蛋】骰子之神似乎不看好你, 你掷出的骰子全部消失了')
-        if dice_num > 1000 or dice_side > 1000:
-            await roll.finish(f'【错误】谁没事干扔那么多骰子啊(╯°□°）╯︵ ┻━┻')
-        if dice_num <= 0 or dice_side <= 0:
-            await roll.finish(f'【错误】你掷出了不存在的骰子, 只有上帝知道结果是多少')
-        dice_result = 0
-        for i in range(dice_num):
-            this_dice_result = random.choice(range(dice_side)) + 1
-            dice_result += this_dice_result
-        await roll.finish(f'你掷出了{dice_num}个{dice_side}面骰子, 点数为【{dice_result}】')
+    elif re.match(r'^\d+$', _roll):
+        dice_num = 1
+        dice_side = int(_roll)
     else:
         await roll.finish(f'格式不对呢, 请重新输入: /roll <x>d<y>:')
+        return
+
+    # 加入一个趣味的机制
+    if random.randint(1, 100) == 99:
+        await roll.finish(f'【彩蛋】骰子之神似乎不看好你, 你掷出的骰子全部消失了')
+    if dice_num > 1024 or dice_side > 1024:
+        await roll.finish(f'【错误】谁没事干扔那么多骰子啊(╯°□°）╯︵ ┻━┻')
+    if dice_num <= 0 or dice_side <= 0:
+        await roll.finish(f'【错误】你掷出了不存在的骰子, 只有上帝知道结果是多少')
+    dice_result = 0
+    for i in range(dice_num):
+        this_dice_result = random.choice(range(dice_side)) + 1
+        dice_result += this_dice_result
+    await roll.finish(f'你掷出了{dice_num}个{dice_side}面骰子, 点数为【{dice_result}】')
 
 
 lottery = Roll.command('lottery', aliases={'抽奖'})
