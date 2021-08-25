273212ba779bd16a2189792dc74917c340dd4661
Ailitonia <ailitonia@gmail.com>
2021-08-26 00:18:48 +0800

    Upgrade: Pixiv插件搜索时合成并发送预览图
    - Pixiv插件搜索时合成并发送预览图
    - 优化Pixiv插件r18权限处理

--- omega_miya/plugins/pixiv/__init__.py
+++ omega_miya/plugins/pixiv/__init__.py
@@ -1,22 +1,28 @@
 import re
+import os
 import asyncio
-import random
+import pathlib
+from math import ceil
 from typing import Optional
+from datetime import datetime
 from nonebot import on_command, logger, get_driver
 from nonebot.plugin.export import export
 from nonebot.typing import T_State
 from nonebot.adapters.cqhttp.bot import Bot
-from nonebot.adapters.cqhttp.event import MessageEvent, GroupMessageEvent, PrivateMessageEvent
+from nonebot.adapters.cqhttp.event import Event, MessageEvent, GroupMessageEvent, PrivateMessageEvent
 from nonebot.adapters.cqhttp.permission import GROUP, PRIVATE_FRIEND
 from nonebot.adapters.cqhttp import MessageSegment, Message
-from omega_miya.database import DBBot
+from omega_miya.database import DBBot, Result
 from omega_miya.utils.omega_plugin_utils import \
     init_export, init_permission_state, PluginCoolDown, PermissionChecker, PicEncoder, MsgSender, ProcessUtils
 from omega_miya.utils.pixiv_utils import PixivIllust
+from PIL import Image, ImageDraw, ImageFont
 from .config import Config
 
 
 __global_config = get_driver().config
+TMP_PATH = __global_config.tmp_path_
+RESOURCES_PATH = __global_config.resources_path_
 plugin_config = Config(**__global_config.dict())
 ENABLE_NODE_CUSTOM = plugin_config.enable_node_custom
 
@@ -106,33 +112,16 @@ async def handle_first_receive(bot: Bot, event: MessageEvent, state: T_State):
 @pixiv.got('mode', prompt='你是想看日榜, 周榜, 月榜, 还是作品呢? 想看特定作品的话请输入PixivID或关键词搜索~')
 async def handle_pixiv(bot: Bot, event: MessageEvent, state: T_State):
     mode = state['mode']
-    if mode == '日榜':
+    if mode in ['日榜', '周榜', '月榜']:
         await pixiv.send('稍等, 正在下载图片~')
-        rank_result = await PixivIllust.get_ranking(mode='daily')
-        if rank_result.error:
-            logger.warning(f"User: {event.user_id} 获取Pixiv Rank失败, {rank_result.info}")
-            await pixiv.finish('加载失败, 网络超时QAQ')
-
-        tasks = []
-        for rank, illust_data in dict(rank_result.result).items():
-            if rank >= 10:
-                break
-            tasks.append(__handle_ranking_msg(rank=rank, illust_data=illust_data))
-        ranking_msg_result = list(await asyncio.gather(*tasks))
-
-        # 根据ENABLE_NODE_CUSTOM处理消息发送
-        if ENABLE_NODE_CUSTOM and isinstance(event, GroupMessageEvent):
-            msg_sender = MsgSender(bot=bot, log_flag='PixivDailyRanking')
-            await msg_sender.safe_send_group_node_custom(group_id=event.group_id, message_list=ranking_msg_result)
+        if mode == '日榜':
+            rank_result = await PixivIllust.get_ranking(mode='daily')
+        elif mode == '周榜':
+            rank_result = await PixivIllust.get_ranking(mode='weekly')
+        elif mode == '月榜':
+            rank_result = await PixivIllust.get_ranking(mode='monthly')
         else:
-            for msg_seg in ranking_msg_result:
-                try:
-                    await pixiv.send(msg_seg)
-                except Exception as e:
-                    logger.warning(f'图片发送失败, user: {event.user_id}. error: {repr(e)}')
-    elif mode == '周榜':
-        await pixiv.send('稍等, 正在下载图片~')
-        rank_result = await PixivIllust.get_ranking(mode='weekly')
+            rank_result = await PixivIllust.get_ranking(mode='daily')
         if rank_result.error:
             logger.warning(f"User: {event.user_id} 获取Pixiv Rank失败, {rank_result.info}")
             await pixiv.finish('加载失败, 网络超时QAQ')
@@ -146,31 +135,7 @@ async def handle_pixiv(bot: Bot, event: MessageEvent, state: T_State):
 
         # 根据ENABLE_NODE_CUSTOM处理消息发送
         if ENABLE_NODE_CUSTOM and isinstance(event, GroupMessageEvent):
-            msg_sender = MsgSender(bot=bot, log_flag='PixivWeeklyRanking')
-            await msg_sender.safe_send_group_node_custom(group_id=event.group_id, message_list=ranking_msg_result)
-        else:
-            for msg_seg in ranking_msg_result:
-                try:
-                    await pixiv.send(msg_seg)
-                except Exception as e:
-                    logger.warning(f'图片发送失败, user: {event.user_id}. error: {repr(e)}')
-    elif mode == '月榜':
-        await pixiv.send('稍等, 正在下载图片~')
-        rank_result = await PixivIllust.get_ranking(mode='monthly')
-        if rank_result.error:
-            logger.warning(f"User: {event.user_id} 获取Pixiv Rank失败, {rank_result.info}")
-            await pixiv.finish('加载失败, 网络超时QAQ')
-
-        tasks = []
-        for rank, illust_data in dict(rank_result.result).items():
-            if rank >= 10:
-                break
-            tasks.append(__handle_ranking_msg(rank=rank, illust_data=illust_data))
-        ranking_msg_result = list(await asyncio.gather(*tasks))
-
-        # 根据ENABLE_NODE_CUSTOM处理消息发送
-        if ENABLE_NODE_CUSTOM and isinstance(event, GroupMessageEvent):
-            msg_sender = MsgSender(bot=bot, log_flag='PixivMonthlyRanking')
+            msg_sender = MsgSender(bot=bot, log_flag='PixivRanking')
             await msg_sender.safe_send_group_node_custom(group_id=event.group_id, message_list=ranking_msg_result)
         else:
             for msg_seg in ranking_msg_result:
@@ -190,17 +155,7 @@ async def handle_pixiv(bot: Bot, event: MessageEvent, state: T_State):
 
         # 处理r18权限
         if illust_data_result.result.get('is_r18'):
-            if isinstance(event, PrivateMessageEvent):
-                user_id = event.user_id
-                auth_checker = await PermissionChecker(self_bot=DBBot(self_qq=int(bot.self_id))).\
-                    check_auth_node(auth_id=user_id, auth_type='user', auth_node='pixiv.allow_r18')
-            elif isinstance(event, GroupMessageEvent):
-                group_id = event.group_id
-                auth_checker = await PermissionChecker(self_bot=DBBot(self_qq=int(bot.self_id))).\
-                    check_auth_node(auth_id=group_id, auth_type='group', auth_node='pixiv.allow_r18')
-            else:
-                auth_checker = 0
-
+            auth_checker = await __handle_r18_perm(bot=bot, event=event)
             if auth_checker != 1:
                 logger.warning(f"User: {event.user_id} 获取Pixiv资源 {pid} 被拒绝, 没有 allow_r18 权限")
                 await pixiv.finish('R18禁止! 不准开车车!')
@@ -230,21 +185,11 @@ async def handle_pixiv(bot: Bot, event: MessageEvent, state: T_State):
         near_year_ = True
         nsfw_ = False
         page_ = 1
-        random_ = False
         if filter_ := re.search(r'^(#(.+?)#)', mode):
             text_ = re.sub(r'^(#(.+?)#)', '', mode).strip()
             filter_text = filter_.groups()[1]
             # 处理r18权限
-            if isinstance(event, PrivateMessageEvent):
-                user_id = event.user_id
-                auth_checker = await PermissionChecker(self_bot=DBBot(self_qq=int(bot.self_id))). \
-                    check_auth_node(auth_id=user_id, auth_type='user', auth_node='pixiv.allow_r18')
-            elif isinstance(event, GroupMessageEvent):
-                group_id = event.group_id
-                auth_checker = await PermissionChecker(self_bot=DBBot(self_qq=int(bot.self_id))). \
-                    check_auth_node(auth_id=group_id, auth_type='group', auth_node='pixiv.allow_r18')
-            else:
-                auth_checker = 0
+            auth_checker = await __handle_r18_perm(bot=bot, event=event)
 
             if 'nsfw' in filter_text:
                 if auth_checker != 1:
@@ -260,9 +205,6 @@ async def handle_pixiv(bot: Bot, event: MessageEvent, state: T_State):
             if '最新' in filter_text:
                 popular_order_ = False
 
-            if '随机' in filter_text:
-                random_ = True
-
             if page_text := re.search(r'第(\d+?)页', filter_text):
                 page_ = int(page_text.groups()[0])
 
@@ -275,34 +217,14 @@ async def handle_pixiv(bot: Bot, event: MessageEvent, state: T_State):
             await pixiv.finish('没有找到相关作品QAQ, 也可能是发生了意外的错误, 请稍后再试~')
         await pixiv.send(f'搜索Pixiv作品: {text_}\n图片下载中, 请稍等~')
 
-        # 选8张用于展示
-        if len(search_result.result) > 8:
-            if random_:
-                choice_illust = random.sample(search_result.result, k=8)
-            else:
-                choice_illust = search_result.result[:8]
-        else:
-            choice_illust = search_result.result
-        # 加载图片
-        tasks = [PicEncoder(pic_url=x.get('thumb_url'), headers=PixivIllust.HEADERS
-                            ).get_file(folder_flag='pixiv_search_thumb') for x in choice_illust]
+        preview_result = await __preview_search_illust(search_result=search_result, title=f'Pixiv - {text_}')
+        if preview_result.error:
+            logger.error(f'生成Pixiv搜索预览图时发生了意外的错误, error: {repr(search_result)}')
+            await pixiv.finish('生成Pixiv搜索预览图时发生了意外的错误QAQ, 请稍后再试~')
 
-        thumb_img_result = await ProcessUtils.fragment_process(tasks=tasks, log_flag='pixiv_search_thumb')
-
-        msg = Message(f'Pixiv - {text_}:\n{"="*12}\n')
-        # 构造消息
-        for index, img_result in enumerate(thumb_img_result):
-            intro = f"Pid:{choice_illust[index].get('pid')}\n" \
-                    f"Title:{choice_illust[index].get('title')}\n" \
-                    f"Author:{choice_illust[index].get('author')}\n"
-            if img_result.error:
-                msg.append(intro).append(MessageSegment.text('-'*12))
-            else:
-                msg.append(intro).append(MessageSegment.image(img_result.result)).append(f"{'-'*12}\n")
-        msg.append('查看作品详情请使用 /pixiv [pid] 命令')
-
+        img_path = pathlib.Path(preview_result.result).as_uri()
         logger.info(f"User: {event.user_id} 搜索了Pixiv作品: {mode}")
-        await pixiv.finish(msg)
+        await pixiv.finish(MessageSegment.image(img_path))
 
 
 # 注册事件响应器
@@ -376,6 +298,22 @@ async def handle_pixiv_dl(bot: Bot, event: GroupMessageEvent, state: T_State):
         await pixiv_dl.finish('参数错误, pid应为纯数字')
 
 
+# 处理pixiv插件r18 权限
+async def __handle_r18_perm(bot: Bot, event: Event) -> int:
+    # 处理pixiv插件r18权限
+    if isinstance(event, PrivateMessageEvent):
+        user_id = event.user_id
+        auth_checker = await PermissionChecker(self_bot=DBBot(self_qq=int(bot.self_id))). \
+            check_auth_node(auth_id=user_id, auth_type='user', auth_node='pixiv.allow_r18')
+    elif isinstance(event, GroupMessageEvent):
+        group_id = event.group_id
+        auth_checker = await PermissionChecker(self_bot=DBBot(self_qq=int(bot.self_id))). \
+            check_auth_node(auth_id=group_id, auth_type='group', auth_node='pixiv.allow_r18')
+    else:
+        auth_checker = 0
+    return auth_checker
+
+
 # 处理Pixiv.__ranking榜单消息
 async def __handle_ranking_msg(rank: int, illust_data: dict) -> Optional[Message]:
     rank += 1
@@ -392,3 +330,75 @@ async def __handle_ranking_msg(rank: int, illust_data: dict) -> Optional[Message
     else:
         logger.warning(f"下载图片失败, pid: {illust_id}, {image_result.info}")
         return None
+
+
+async def __preview_search_illust(
+        search_result: Result.DictListResult,
+        title: str,
+        *,
+        line_num: int = 6) -> Result.TextResult:
+    """
+    拼接pixiv作品预览图, 固定缩略图分辨率250*250
+    :param search_result: 搜索结果
+    :param title: 生成图片标题
+    :param line_num: 单行作品数
+    :return: 拼接后图片位置
+    """
+    illust_list = search_result.result
+    # 加载图片
+    tasks = [PicEncoder(pic_url=x.get('thumb_url'), headers=PixivIllust.HEADERS
+                        ).get_file(folder_flag='pixiv_search_thumb') for x in illust_list]
+    thumb_img_result = await ProcessUtils.fragment_process(tasks=tasks, fragment_size=20, log_flag='pixiv_search_thumb')
+    if not thumb_img_result:
+        return Result.TextResult(error=True, info='Not result', result='')
+
+    def __handle() -> str:
+        size = (250, 250)
+        thumb_w, thumb_h = size
+        font_path = os.path.abspath(os.path.join(RESOURCES_PATH, 'fonts', 'fzzxhk.ttf'))
+        font_main = ImageFont.truetype(font_path, thumb_w // 15)
+        background = Image.new(
+            mode="RGB",
+            size=(thumb_w * line_num, (thumb_h + 100) * ceil(len(thumb_img_result) / line_num) + 100),
+            color=(255, 255, 255))
+        # 写标题
+        ImageDraw.Draw(background).text(
+            xy=((thumb_w * line_num) // 2, 20), text=title, font=ImageFont.truetype(font_path, thumb_w // 5),
+            spacing=8, align='center', anchor='ma', fill=(0, 0, 0))
+
+        # 处理拼图
+        line = 0
+        for index, img_result in enumerate(thumb_img_result):
+            # 处理单个缩略图
+            draw_: Image.Image = Image.open(re.sub(r'^file:///', '', img_result.result))
+            if draw_.size != size:
+                draw_.resize(size)
+
+            # 确认缩略图单行位置
+            seq = index % line_num
+            # 能被整除说明在行首要换行
+            if seq == 0:
+                line += 1
+            # 按位置粘贴单个缩略图
+            background.paste(draw_, box=(seq * thumb_w, (thumb_h + 100) * (line - 1) + 100))
+            pid_text = f"Pid: {illust_list[index].get('pid')}"
+            title_text = f"{illust_list[index].get('title')}"
+            title_text = f"{title_text[:13]}..." if len(title_text) > 13 else title_text
+            author_text = f"Author: {illust_list[index].get('author')}"
+            author_text = f"{author_text[:13]}..." if len(author_text) > 13 else author_text
+            text = f'{pid_text}\n{title_text}\n{author_text}'
+            ImageDraw.Draw(background).multiline_text(
+                xy=(seq * thumb_w + thumb_w // 2, (thumb_h + 100) * line + 10), text=text, font=font_main,
+                spacing=8, align='center', anchor='ma', fill=(0, 0, 0))
+
+        save_path = os.path.abspath(os.path.join(
+            TMP_PATH, 'pixiv_search_thumb', f"preview_search_{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}.jpg"))
+        background.save(save_path, 'JPEG')
+        return save_path
+
+    try:
+        loop = asyncio.get_running_loop()
+        result = await loop.run_in_executor(None, __handle)
+        return Result.TextResult(error=False, info='Success', result=result)
+    except Exception as e:
+        return Result.TextResult(error=True, info=repr(e), result='')
