ac31da118ab38b1cf866d53756409b7573debd96
Ailitonia <ailitonia@gmail.com>
2021-08-27 05:52:40 +0800

    Upgrade: 移除dd老黄历功能，新增"帮我选"功能
    - 移除dd老黄历功能
    - 新增"帮我选"功能

--- omega_miya/plugins/maybe/__init__.py
+++ omega_miya/plugins/maybe/__init__.py
@@ -1,13 +1,15 @@
 import datetime
+import random
 from nonebot import CommandGroup, logger
 from nonebot.plugin.export import export
 from nonebot.typing import T_State
 from nonebot.adapters.cqhttp.bot import Bot
 from nonebot.adapters.cqhttp.event import GroupMessageEvent
 from nonebot.adapters.cqhttp.permission import GROUP
+from nonebot.adapters.cqhttp.message import Message, MessageSegment
 from omega_miya.utils.omega_plugin_utils import init_export, init_permission_state
 from .utils import maybe, sp,  sp_event
-from .oldalmanac import old_almanac
+# from .oldalmanac import old_almanac
 
 
 # Custom plugin usage text
@@ -27,7 +29,7 @@ basic
 
 **Usage**
 /求签 [所求之事]
-/DD老黄历'''
+/帮我选 [选项1 选项2 ...]'''
 
 # 声明本插件可配置的权限节点
 __plugin_auth_node__ = [
@@ -56,7 +58,7 @@ luck = Maybe.command('luck', aliases={'求签'})
 # 修改默认参数处理
 @luck.args_parser
 async def parse(bot: Bot, event: GroupMessageEvent, state: T_State):
-    args = str(event.get_plaintext()).strip().lower().split()
+    args = str(event.get_plaintext()).strip().split()
     if not args:
         await luck.reject('你似乎没有发送有效的参数呢QAQ, 请重新发送:')
     state[state["_current_key"]] = args[0]
@@ -66,7 +68,7 @@ async def parse(bot: Bot, event: GroupMessageEvent, state: T_State):
 
 @luck.handle()
 async def handle_first_receive(bot: Bot, event: GroupMessageEvent, state: T_State):
-    args = str(event.get_plaintext()).strip().lower().split()
+    args = str(event.get_plaintext()).strip().split()
     if not args:
         pass
     elif args and len(args) == 1:
@@ -97,28 +99,60 @@ async def handle_luck(bot: Bot, event: GroupMessageEvent, state: T_State):
     await luck.finish(msg)
 
 
-almanac = Maybe.command('almanac', aliases={'DD老黄历', 'dd老黄历'})
+help_choice = Maybe.command('choice', aliases={'帮我选', '选择困难症'})
 
 
-@almanac.handle()
+# 修改默认参数处理
+@help_choice.args_parser
+async def parse(bot: Bot, event: GroupMessageEvent, state: T_State):
+    args = str(event.get_plaintext()).strip()
+    if not args:
+        await help_choice.reject('你似乎没有发送有效的参数呢QAQ, 请重新发送:')
+    state[state["_current_key"]] = args
+    if state[state["_current_key"]] == '取消':
+        await help_choice.finish('操作已取消')
+
+
+@help_choice.handle()
 async def handle_first_receive(bot: Bot, event: GroupMessageEvent, state: T_State):
-    args = str(event.get_plaintext()).strip().lower().split()
+    args = str(event.get_plaintext()).strip()
     if not args:
         pass
     else:
-        await almanac.finish('参数错误QAQ')
+        state['choices'] = args
 
-    user_id = event.user_id
 
-    # 求签者昵称, 优先使用群昵称
-    draw_user = event.sender.card
-    if not draw_user:
-        draw_user = event.sender.nickname
+@help_choice.got('choices', prompt='有啥选项, 发来我帮你选~')
+async def handle_help_choice(bot: Bot, event: GroupMessageEvent, state: T_State):
+    choices = state['choices']
+    result = random.choice(str(choices).split())
+    result_text = '帮你从"' + '","'.join(str(choices).split()) + f'"中选择了: "{result}"'
+    msg = Message(MessageSegment.at(user_id=event.user_id)).append(result_text)
+    await help_choice.finish(msg)
 
-    draw_result = old_almanac(user_id=user_id)
 
-    # 向用户发送结果
-    today = datetime.date.today().strftime('%Y年%m月%d日')
-    msg = f"今天是{today}\n{draw_user}今日:\n{'='*12}\n{draw_result}"
-    logger.info(f'{event.group_id}/{event.user_id} 进行了一次dd老黄历查询')
-    await almanac.finish(msg)
+# almanac = Maybe.command('almanac', aliases={'DD老黄历', 'dd老黄历'})
+#
+#
+# @almanac.handle()
+# async def handle_first_receive(bot: Bot, event: GroupMessageEvent, state: T_State):
+#     args = str(event.get_plaintext()).strip().lower().split()
+#     if not args:
+#         pass
+#     else:
+#         await almanac.finish('参数错误QAQ')
+#
+#     user_id = event.user_id
+#
+#     # 求签者昵称, 优先使用群昵称
+#     draw_user = event.sender.card
+#     if not draw_user:
+#         draw_user = event.sender.nickname
+#
+#     draw_result = old_almanac(user_id=user_id)
+#
+#     # 向用户发送结果
+#     today = datetime.date.today().strftime('%Y年%m月%d日')
+#     msg = f"今天是{today}\n{draw_user}今日:\n{'='*12}\n{draw_result}"
+#     logger.info(f'{event.group_id}/{event.user_id} 进行了一次dd老黄历查询')
+#     await almanac.finish(msg)
