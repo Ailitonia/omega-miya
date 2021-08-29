c1f7bb373a46192e769752d40049bb978f5b2f3f
Ailitonia <ailitonia@gmail.com>
2021-08-30 02:07:28 +0800

    Upgrade: 优化了rule对群组事件的判断
    - 优化了rule对群组事件的判断

--- omega_miya/utils/omega_plugin_utils/rules.py
+++ omega_miya/utils/omega_plugin_utils/rules.py
@@ -1,7 +1,7 @@
 from nonebot.rule import Rule
 from nonebot.typing import T_State
 from nonebot.adapters.cqhttp.bot import Bot
-from nonebot.adapters.cqhttp.event import Event, MessageEvent, NoticeEvent, RequestEvent
+from nonebot.adapters.cqhttp.event import Event, MessageEvent, PrivateMessageEvent, NoticeEvent, RequestEvent
 from omega_miya.database import DBBot, DBFriend, DBBotGroup, DBAuth
 
 
@@ -11,18 +11,12 @@ class OmegaRules(object):
     @classmethod
     def has_group_notice_permission(cls) -> Rule:
         async def _has_group_notice_permission(bot: Bot, event: Event, state: T_State) -> bool:
-            if isinstance(event, MessageEvent):
-                detail_type = event.message_type
-            elif isinstance(event, NoticeEvent):
-                detail_type = event.notice_type
-            elif isinstance(event, RequestEvent):
-                detail_type = event.request_type
-            else:
-                detail_type = None
+            if isinstance(event, PrivateMessageEvent):
+                return False
             group_id = getattr(event, 'group_id', None)
             self_bot = DBBot(self_qq=int(bot.self_id))
             # 检查当前消息类型
-            if not str(detail_type).startswith('group'):
+            if not group_id:
                 return False
             else:
                 res = await DBBotGroup(group_id=group_id, self_bot=self_bot).permission_notice()
@@ -35,18 +29,12 @@ class OmegaRules(object):
     @classmethod
     def has_group_command_permission(cls) -> Rule:
         async def _has_group_command_permission(bot: Bot, event: Event, state: T_State) -> bool:
-            if isinstance(event, MessageEvent):
-                detail_type = event.message_type
-            elif isinstance(event, NoticeEvent):
-                detail_type = event.notice_type
-            elif isinstance(event, RequestEvent):
-                detail_type = event.request_type
-            else:
-                detail_type = None
+            if isinstance(event, PrivateMessageEvent):
+                return False
             group_id = getattr(event, 'group_id', None)
             self_bot = DBBot(self_qq=int(bot.self_id))
             # 检查当前消息类型
-            if not str(detail_type).startswith('group'):
+            if not group_id:
                 return False
             else:
                 res = await DBBotGroup(group_id=group_id, self_bot=self_bot).permission_command()
@@ -59,18 +47,12 @@ class OmegaRules(object):
     @classmethod
     def has_group_permission_level(cls, level: int) -> Rule:
         async def _has_group_permission_level(bot: Bot, event: Event, state: T_State) -> bool:
-            if isinstance(event, MessageEvent):
-                detail_type = event.message_type
-            elif isinstance(event, NoticeEvent):
-                detail_type = event.notice_type
-            elif isinstance(event, RequestEvent):
-                detail_type = event.request_type
-            else:
-                detail_type = None
+            if isinstance(event, PrivateMessageEvent):
+                return False
             group_id = getattr(event, 'group_id', None)
             self_bot = DBBot(self_qq=int(bot.self_id))
             # 检查当前消息类型
-            if not str(detail_type).startswith('group'):
+            if not group_id:
                 return False
             else:
                 res = await DBBotGroup(group_id=group_id, self_bot=self_bot).permission_level()
@@ -85,24 +67,16 @@ class OmegaRules(object):
     def has_auth_node(cls, *auth_nodes: str) -> Rule:
         async def _has_auth_node(bot: Bot, event: Event, state: T_State) -> bool:
             auth_node = '.'.join(auth_nodes)
-            if isinstance(event, MessageEvent):
-                detail_type = event.message_type
-            elif isinstance(event, NoticeEvent):
-                detail_type = event.notice_type
-            elif isinstance(event, RequestEvent):
-                detail_type = event.request_type
-            else:
-                detail_type = None
             group_id = getattr(event, 'group_id', None)
             user_id = getattr(event, 'user_id', None)
             self_bot = DBBot(self_qq=int(bot.self_id))
             # 检查当前消息类型
-            if detail_type == 'private':
+            if isinstance(event, PrivateMessageEvent):
                 user_auth = DBAuth(self_bot=self_bot, auth_id=user_id, auth_type='user', auth_node=auth_node)
                 user_tag_res = await user_auth.tags_info()
                 allow_tag = user_tag_res.result[0]
                 deny_tag = user_tag_res.result[1]
-            elif str(detail_type).startswith('group'):
+            elif group_id is not None:
                 group_auth = DBAuth(self_bot=self_bot, auth_id=group_id, auth_type='group', auth_node=auth_node)
                 group_tag_res = await group_auth.tags_info()
                 allow_tag = group_tag_res.result[0]
@@ -127,20 +101,12 @@ class OmegaRules(object):
         """
         async def _has_level_or_node(bot: Bot, event: Event, state: T_State) -> bool:
             auth_node = '.'.join(auth_nodes)
-            if isinstance(event, MessageEvent):
-                detail_type = event.message_type
-            elif isinstance(event, NoticeEvent):
-                detail_type = event.notice_type
-            elif isinstance(event, RequestEvent):
-                detail_type = event.request_type
-            else:
-                detail_type = None
             group_id = getattr(event, 'group_id', None)
             user_id = getattr(event, 'user_id', None)
             self_bot = DBBot(self_qq=int(bot.self_id))
 
             # level检查部分
-            if detail_type != 'group':
+            if not group_id:
                 level_checker = False
             else:
                 level_res = await DBBotGroup(group_id=group_id, self_bot=self_bot).permission_level()
@@ -150,12 +116,12 @@ class OmegaRules(object):
                     level_checker = False
 
             # node检查部分
-            if detail_type == 'private':
+            if isinstance(event, PrivateMessageEvent):
                 user_auth = DBAuth(self_bot=self_bot, auth_id=user_id, auth_type='user', auth_node=auth_node)
                 user_tag_res = await user_auth.tags_info()
                 allow_tag = user_tag_res.result[0]
                 deny_tag = user_tag_res.result[1]
-            elif str(detail_type).startswith('group'):
+            elif group_id is not None:
                 group_auth = DBAuth(self_bot=self_bot, auth_id=group_id, auth_type='group', auth_node=auth_node)
                 group_tag_res = await group_auth.tags_info()
                 allow_tag = group_tag_res.result[0]
