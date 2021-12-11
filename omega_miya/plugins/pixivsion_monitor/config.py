6f6540552f810d9d0aeb09c7053d5fd85d0bdb56
Ailitonia <ailitonia@gmail.com>
2021-12-11 20:35:51 +0800

    Change: 修改 pixivision 插件默认发送配置
    - 修改 pixivision 插件默认发送配置

--- omega_miya/plugins/pixivsion_monitor/config.py
+++ omega_miya/plugins/pixivsion_monitor/config.py
@@ -16,7 +16,7 @@ class Config(BaseSettings):
     # plugin custom config
     # 启用使用群组转发自定义消息节点的模式发送信息
     # 发送速度受限于网络上传带宽, 有可能导致超时或发送失败, 请酌情启用
-    enable_node_custom: bool = False
+    enable_node_custom: bool = True
     sub_id: str = 'pixivision'
 
     # 不推送包含以下 tag 的 article
