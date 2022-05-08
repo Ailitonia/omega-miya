a535b1334232ea55505257afb30d764b862a2a8c
Ailitonia <ailitonia@gmail.com>
2022-05-09 01:01:03 +0800

    Refactor: self_mute plugin
    - 重构 self_mute 插件

--- omega_miya/plugins/self_mute/__init__.py
+++ omega_miya/plugins/self_mute/__init__.py
@@ -11,12 +11,17 @@
 import re
 import random
 from nonebot import on_command, logger
-from nonebot.plugin.export import export
 from nonebot.typing import T_State
-from nonebot.adapters.cqhttp.bot import Bot
-from nonebot.adapters.cqhttp.event import GroupMessageEvent
-from nonebot.adapters.cqhttp.permission import GROUP
-from omega_miya.utils.omega_plugin_utils import init_export, init_processor_state, RoleChecker
+from nonebot.matcher import Matcher
+from nonebot.adapters.onebot.v11.bot import Bot
+from nonebot.adapters.onebot.v11.event import GroupMessageEvent
+from nonebot.adapters.onebot.v11.message import Message
+from nonebot.adapters.onebot.v11.permission import GROUP
+from nonebot.params import Depends, CommandArg, Arg
+
+from omega_miya.service import init_processor_state
+from omega_miya.utils.process_utils import run_async_catching_exception
+from omega_miya.onebot_api import GoCqhttpBot
 
 
 # Custom plugin usage text
@@ -24,56 +29,66 @@ __plugin_custom_name__ = '随机口球'
 __plugin_usage__ = r'''【随机口球】
 自取随机口球礼包
 
-**Permission**
-Friend Private
-Command & Lv.10
-or AuthNode
-
-**AuthNode**
-basic
-
-**Usage**
+用法:
 /随机口球 [n倍]'''
-
-
-# Init plugin export
-init_export(export(), __plugin_custom_name__, __plugin_usage__)
 
 
 # 注册事件响应器
 self_mute = on_command(
     'SelfMute',
     # 使用run_preprocessor拦截权限管理, 在default_state初始化所需权限
-    state=init_processor_state(
-        name='self_mute',
-        command=True,
-        level=10),
+    state=init_processor_state(name='self_mute', level=10),
     aliases={'随机口球', '口球', '禁言礼包'},
     permission=GROUP,
     priority=10,
-    block=True)
+    block=True
+)
+
+
+@Depends
+async def role_checker(bot: Bot, event: GroupMessageEvent, matcher: Matcher):
+    gocq_bot = GoCqhttpBot(bot=bot)
+    bot_role = await run_async_catching_exception(gocq_bot.get_group_member_info)(group_id=event.group_id,
+                                                                                  user_id=bot.self_id)
+    if bot_role.role not in ['owner', 'admin']:
+        await matcher.finish('Bot非群管理员, 无法执行禁言操作QAQ')
+
+    user_role = await run_async_catching_exception(gocq_bot.get_group_member_info)(group_id=event.group_id,
+                                                                                   user_id=event.user_id)
+    if user_role.role in ['owner', 'admin']:
+        await matcher.finish('你也是个管理, 别来凑热闹OwO')
+
+
+@Depends
+async def parse_multiple(message: Message | str = Arg('multiple')) -> int:
+    if isinstance(message, Message):
+        message = message.extract_plain_text().strip()
+    if multiple_text := re.search(r'^(\d+)倍$', message):
+        multiple = int(multiple_text.group(1))
+    elif message.isdigit():
+        multiple = int(message)
+    else:
+        multiple = 1
+    return multiple
 
 
 @self_mute.handle()
-async def handle_self_mute(bot: Bot, event: GroupMessageEvent, state: T_State):
-    args = str(event.get_plaintext()).strip()
-    # 检查是否有倍数参数
-    if multiple_text := re.search(r'^(\d+)倍$', args):
-        multiple = int(multiple_text.groups()[0])
+async def handle_parser_multiple(state: T_State, cmd_arg: Message = CommandArg()):
+    """首次运行时解析命令参数"""
+    multiple = cmd_arg.extract_plain_text().strip()
+    if multiple:
+        state.update({'multiple': multiple})
     else:
-        multiple = 1
+        state.update({'multiple': '1'})
 
-    # 检查bot和用户身份
-    if not (await RoleChecker(group_id=event.group_id, user_id=event.self_id, bot=bot).is_group_admin()):
-        await self_mute.finish('Bot非群管理员, 无法执行禁言操作QAQ')
-    if await RoleChecker(group_id=event.group_id, user_id=event.user_id, bot=bot).is_group_admin():
-        await self_mute.finish('你也是个管理, 别来凑热闹OvO', at_sender=True)
 
+@self_mute.got('multiple', parameterless=[role_checker])
+async def handle_self_mute(bot: Bot, event: GroupMessageEvent, multiple: int = parse_multiple):
     # 随机禁言时间
     random_time = 2 * int(random.gauss(128 * multiple, 640 * multiple // 10))
     act_time = 60 if random_time < 60 else (random_time if random_time < 2591940 else 2591940)
     msg = f'既然你那么想被口球的话, 那我就成全你吧!\n送你一份{act_time // 60}分{act_time % 60}秒的禁言套餐哦, 谢谢惠顾~'
 
     await bot.set_group_ban(group_id=event.group_id, user_id=event.user_id, duration=act_time)
-    logger.info(f'Group: {event.group_id}, User: {event.user_id} 抽取了 {act_time} 秒的禁言套餐')
+    logger.info(f'SelfMute | Group({event.group_id})/User({event.user_id}) 被自己禁言 {act_time} 秒')
     await self_mute.finish(msg, at_sender=True)
