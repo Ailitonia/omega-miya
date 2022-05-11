c1e35b96db96692ede63b8a6a68eed8975cee668
Ailitonia <ailitonia@gmail.com>
2022-05-12 02:06:01 +0800

    Add: import tool from v0.8.2
    - Add import tool from v0.8.2

--- bot.py
+++ bot.py
@@ -16,18 +16,18 @@ if not os.path.exists(bot_log_path):
     os.makedirs(bot_log_path)
 
 # Custom logger
-log_info_name = f"{datetime.today().strftime('%Y%m%d-%H%M%S')}-INFO.log"
-log_error_name = f"{datetime.today().strftime('%Y%m%d-%H%M%S')}-ERROR.log"
+log_info_name = f'{datetime.today().strftime("%Y%m%d-%H%M%S")}-INFO.log'
+log_error_name = f'{datetime.today().strftime("%Y%m%d-%H%M%S")}-ERROR.log'
 log_info_path = os.path.join(bot_log_path, log_info_name)
 log_error_path = os.path.join(bot_log_path, log_error_name)
 
-logger.add(log_info_path, rotation="00:00", diagnose=False, level="INFO", format=default_format, encoding='utf-8')
-logger.add(log_error_path, rotation="00:00", diagnose=False, level="ERROR", format=default_format, encoding='utf-8')
+logger.add(log_info_path, rotation='00:00', diagnose=False, level='INFO', format=default_format, encoding='utf-8')
+logger.add(log_error_path, rotation='00:00', diagnose=False, level='ERROR', format=default_format, encoding='utf-8')
 
 # Add extra debug log file
-# log_debug_name = f"{datetime.today().strftime('%Y%m%d-%H%M%S')}-DEBUG.log"
+# log_debug_name = f'{datetime.today().strftime("%Y%m%d-%H%M%S")}-DEBUG.log'
 # log_debug_path = os.path.join(bot_log_path, log_debug_name)
-# logger.add(log_debug_path, rotation="00:00", diagnose=False, level="DEBUG", format=default_format, encoding='utf-8')
+# logger.add(log_debug_path, rotation='00:00', diagnose=False, level='DEBUG', format=default_format, encoding='utf-8')
 
 # You can pass some keyword args config to init function
 nonebot.init()
@@ -39,14 +39,15 @@ driver = nonebot.get_driver()
 driver.register_adapter(OneBotAdapter)
 
 # 加载插件
-nonebot.load_plugins("omega_miya/utils")
-nonebot.load_plugins("omega_miya/service")
-nonebot.load_plugins("omega_miya/plugins")
+nonebot.load_plugins('omega_miya/utils')
+nonebot.load_plugins('omega_miya/service')
+nonebot.load_plugins('omega_miya/plugins')
+# nonebot.load_plugin('omega_miya.plugins.import_old_version_data')
 
 # Modify some config / config depends on loaded configs
 # config = nonebot.get_driver().config
 # do something...
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     nonebot.run()
