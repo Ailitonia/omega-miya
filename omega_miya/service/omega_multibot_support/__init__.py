cbf88d535ff595d20f73a5f94714432829fa6d66
Ailitonia <ailitonia@gmail.com>
2022-05-15 17:06:17 +0800

    Fix: 对于多协议端同时接入, 修复多个协议端在同一个群内响应混乱的问题
    - 对于多协议端同时接入, 修复多个协议端在同一个群内响应混乱的问题

--- omega_miya/service/omega_multibot_support/__init__.py
+++ omega_miya/service/omega_multibot_support/__init__.py
@@ -1,17 +1,19 @@
 """
 @Author         : Ailitonia
 @Date           : 2021/05/23 19:40
-@FileName       : __init__.py
+@FileName       : omega_multibot_support.py
 @Project        : nonebot2_miya 
 @Description    : Multi-Bot 多协议端接入支持
 @GitHub         : https://github.com/Ailitonia
 @Software       : PyCharm 
 """
 
-from typing import Dict
-from nonebot import get_driver, logger
+from nonebot import get_driver
+from nonebot.log import logger
+from nonebot.matcher import Matcher
+from nonebot.permission import Permission
 from nonebot.adapters.onebot.v11.bot import Bot
-from nonebot.adapters.onebot.v11.event import Event
+from nonebot.adapters.onebot.v11.event import Event, MessageEvent, NoticeEvent, RequestEvent
 from nonebot.message import run_preprocessor
 from nonebot.exception import IgnoredException
 
@@ -19,7 +21,38 @@ from omega_miya.onebot_api import GoCqhttpBot
 
 
 driver = get_driver()
-_ONLINE_BOTS: Dict[str, Bot] = {}
+_ONLINE_BOTS: dict[str, Bot] = {}
+"""当前在线的 Bot"""
+
+
+class OriginalResponding:
+    """检查当前事件是否属于由最初响应的 Bot 发起的指定会话
+
+    参数:
+        sessions: 会话 ID 元组
+        original: 最初响应的 Bot
+        perm: 需同时满足的权限
+    """
+
+    __slots__ = ('sessions', 'original', 'perm')
+
+    def __init__(self, sessions: tuple[str, ...], original: str | None = None, perm: Permission | None = None) -> None:
+        self.sessions = sessions
+        self.original = original
+        self.perm = perm
+
+    async def __call__(self, bot: Bot, event: Event) -> bool:
+        return bool(
+            event.get_session_id() in self.sessions
+            and (self.original is None or bot.self_id == self.original)
+            and (self.perm is None or await self.perm(bot, event))
+        )
+
+
+async def original_responding_permission_updater(bot: Bot, event: Event, matcher: Matcher) -> Permission:
+    """匹配当前事件是否属于由最初响应的 Bot 发起的指定会话"""
+    return Permission(OriginalResponding(sessions=(event.get_session_id(),),
+                                         original=bot.self_id, perm=matcher.permission))
 
 
 @driver.on_bot_connect
@@ -49,7 +82,7 @@ async def disconnected_bot_database_upgrade(bot: Bot):
 
 
 @run_preprocessor
-async def unique_bot_responding_limit(bot: Bot, event: Event):
+async def unique_bot_responding_limit(bot: Bot, event: Event, matcher: Matcher):
     # 对于多协议端同时接入, 需匹配event.self_id与bot.self_id, 以保证会话不会被跨bot, 跨群, 跨用户触发
     if bot.self_id != str(event.self_id):
         logger.debug(f'Bot {bot.self_id} ignored event which not match self_id with {event.self_id}.')
@@ -60,3 +93,8 @@ async def unique_bot_responding_limit(bot: Bot, event: Event):
     if event_user_id in [x for x in _ONLINE_BOTS.keys() if x != bot.self_id]:
         logger.debug(f'Bot {bot.self_id} ignored responding self-relation event with Bot {event_user_id}.')
         raise IgnoredException(f'Bot {bot.self_id} ignored responding self-relation event with Bot {event_user_id}.')
+
+    # 对于多协议端同时接入, 需要使用 permission_updater 限制 bot 的 self_id 避免响应混乱
+    if isinstance(event, (MessageEvent, NoticeEvent, RequestEvent)) and not matcher.temp:
+        if not matcher.__class__._default_permission_updater:
+            matcher.permission_updater(original_responding_permission_updater)
