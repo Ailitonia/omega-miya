4464bc1e43aeb2ce68a4a750af7f86d55c2a86df
Ailitonia <ailitonia@gmail.com>
2022-05-22 20:47:06 +0800

    Add: Guild Superuser Permission
    - Add guild superuser permission

--- omega_miya/service/gocqhttp_guild_patch/permission.py
+++ omega_miya/service/gocqhttp_guild_patch/permission.py
@@ -9,6 +9,7 @@
 """
 
 from nonebot.permission import Permission
+from nonebot.adapters.onebot.v11.bot import Bot
 from .models import GuildMessageEvent
 
 
@@ -16,12 +17,19 @@ async def _guild(event: GuildMessageEvent) -> bool:
     return True
 
 
-GUILD = Permission(_guild)
-"""
-- **说明**: 匹配任意频道消息类型事件
-"""
+async def _guild_superuser(bot: Bot, event: GuildMessageEvent) -> bool:
+    return (f"{bot.adapter.get_name().split(maxsplit=1)[0].lower()}:{event.get_user_id()}"
+            in bot.config.superusers
+            or event.get_user_id() in bot.config.superusers)  # 兼容旧配置
+
+
+GUILD: Permission = Permission(_guild)
+"""匹配任意频道消息类型事件"""
+GUILD_SUPERUSER: Permission = Permission(_guild_superuser)
+"""匹配任意超级用户频道消息类型事件"""
 
 
 __all__ = [
-    'GUILD'
+    'GUILD',
+    'GUILD_SUPERUSER'
 ]
