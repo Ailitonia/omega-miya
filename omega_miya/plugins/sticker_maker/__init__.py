5d3ec10fd15a676c80905b9fbdbe4c5693817219
Ailitonia <ailitonia@gmail.com>
2022-05-26 23:14:06 +0800

    Upgrade: 表情包插件支持命令中 @ 人作为素材参数了
    - 表情包插件支持命令中 @ 人作为素材参数了

--- omega_miya/plugins/sticker_maker/__init__.py
+++ omega_miya/plugins/sticker_maker/__init__.py
@@ -1,14 +1,19 @@
 from nonebot import on_command, logger
 from nonebot.typing import T_State
 from nonebot.matcher import Matcher
+from nonebot.adapters.onebot.v11.bot import Bot
+from nonebot.adapters.onebot.v11.event import MessageEvent, GroupMessageEvent
 from nonebot.adapters.onebot.v11.message import Message, MessageSegment
 from nonebot.adapters.onebot.v11.permission import GROUP, PRIVATE_FRIEND
 from nonebot.params import Depends, CommandArg, Arg, ArgStr
 
 from omega_miya.service import init_processor_state
+from omega_miya.service.gocqhttp_guild_patch import GuildMessageEvent
 from omega_miya.service.gocqhttp_guild_patch.permission import GUILD
+from omega_miya.onebot_api import GoCqhttpBot
 from omega_miya.utils.process_utils import run_async_catching_exception
 from omega_miya.utils.message_tools import MessageTools
+from omega_miya.utils.qq_tools import get_user_head_img_url
 
 from .render import get_render, get_all_render_name, download_source_image
 
@@ -48,26 +53,52 @@ def parse_image(key: str):
     return _key_parser
 
 
-@sticker.handle()
+@Depends
+async def parse_at_head_image(bot: Bot, event: MessageEvent, state: T_State, cmd_message: Message = CommandArg()):
+    """解析命令消息中 @ 人的信息, 并将最后被 @ 人的信息作为素材保存到 state 中"""
+    at_list = MessageTools(message=cmd_message).get_all_at_qq()
+    if at_list:
+        gocq_bot = GoCqhttpBot(bot=bot)
+        if isinstance(event, GuildMessageEvent):
+            profile = await run_async_catching_exception(gocq_bot.get_guild_member_profile)(
+                guild_id=event.guild_id, user_id=at_list[-1])
+            if not isinstance(profile, Exception):
+                state.update({'source_image': profile.avatar_url, 'text': profile.nickname})
+        elif isinstance(event, GroupMessageEvent):
+            avatar_url = get_user_head_img_url(user_id=at_list[-1])
+            nickname = ''
+            profile = await run_async_catching_exception(gocq_bot.get_group_member_info)(
+                group_id=event.group_id, user_id=at_list[-1])
+            if not isinstance(profile, Exception):
+                nickname = profile.card if profile.card else profile.nickname
+            state.update({'source_image': avatar_url, 'text': nickname})
+        else:
+            avatar_url = get_user_head_img_url(user_id=at_list[-1])
+            state.update({'source_image': avatar_url})
+
+
+@sticker.handle(parameterless=[parse_at_head_image])
 async def handle_parser(matcher: Matcher, state: T_State, cmd_arg: Message = CommandArg()):
     """首次运行时解析命令参数"""
     # 解析消息中是否有图片
     images = MessageTools(message=cmd_arg).get_all_img_url()
     if images:
         state.update({'source_image': images[0]})
-    else:
+
+    if not state.get('source_image'):
         state.update({'source_image': ''})
+    if not state.get('text'):
+        state.update({'text': ''})
 
     # 处理消息中其他参数
     cmd_args = cmd_arg.extract_plain_text().strip().split(maxsplit=1)
     arg_num = len(cmd_args)
     match arg_num:
         case 1:
-            state.update({'render_name': cmd_args[0], 'text': ''})
+            state.update({'render_name': cmd_args[0]})
         case 2:
             state.update({'render_name': cmd_args[0], 'text': cmd_args[1]})
         case _:
-            state.update({'text': ''})
             render_name_text = '当前可用表情包模板有:\n\n' + '\n'.join(x for x in get_all_render_name())
             await matcher.send(render_name_text)
 
