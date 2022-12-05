302ddf761fc4ad4c1276f796e94efbbba7adc1b9
Ailitonia <ailitonia@gmail.com>
2022-12-05 22:32:29 +0800

    Add: Omega 基础服务
    - Omega 基础服务

---
+++ src/service/omega_base/__init__.py
@@ -0,0 +1,9 @@
+"""
+@Author         : Ailitonia
+@Date           : 2022/12/05 22:22
+@FileName       : omega_base.py
+@Project        : nonebot2_miya 
+@Description    : Omega 基础服务, 封装了常用的数据库操作, 和一些常用工具类函数
+@GitHub         : https://github.com/Ailitonia
+@Software       : PyCharm 
+"""
