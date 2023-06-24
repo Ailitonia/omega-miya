132c84f1bb5b93ac7ede3fd740a4b65ae8d416be
Ailitonia <41713304+Ailitonia@users.noreply.github.com>
2023-06-24 23:20:20 +0800

    Deactivated: Message tools
    - Deactivated message tools

--- src/utils/message_tools/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-@Author         : Ailitonia
-@Date           : 2021/08/14 19:42
-@FileName       : message_tools.py
-@Project        : nonebot2_miya 
-@Description    : 消息工具集
-@GitHub         : https://github.com/Ailitonia
-@Software       : PyCharm 
-"""
