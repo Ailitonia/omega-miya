8bf73a340c8affb81450ec7e5b772330d56329e0
Ailitonia <41713304+Ailitonia@users.noreply.github.com>
2023-06-24 23:38:10 +0800

    Upgrade: Apscheduler
    - Upgrade apscheduler

--- src/service/apscheduler/__init__.py
+++ src/service/apscheduler/__init__.py
@@ -1,11 +1,28 @@
 import logging
 
 from nonebot import get_driver
+from nonebot.plugin import PluginMetadata
 from nonebot.log import LoguruHandler, logger
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 from .config import Config
 from .utils import reschedule_job
+
+
+__plugin_meta__ = PluginMetadata(
+    name="定时任务",
+    description="APScheduler 定时任务插件",
+    usage=(
+        '声明依赖: `require("nonebot_plugin_apscheduler")\n'
+        "导入调度器: `from nonebot_plugin_apscheduler import scheduler`\n"
+        "添加任务: `scheduler.add_job(...)`\n"
+    ),
+    type="library",
+    homepage="https://github.com/nonebot/plugin-apscheduler",
+    config=Config,
+    supported_adapters=None,
+)
+
 
 driver = get_driver()
 global_config = driver.config
