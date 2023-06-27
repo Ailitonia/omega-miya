b4b45a57909f55a1d5221e5b670a45284acd34f0
Ailitonia <41713304+Ailitonia@users.noreply.github.com>
2023-06-28 00:11:17 +0800

    Revert "Merge branch 'dev_refactor_v1' into master"
    This reverts commit b3603e17e1435d3a363accce66c7a6e6e7144836, reversing
    changes made to 5d5a602517ab692c22e9aaa655eb2be9edc133ba.

--- src/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-@Author         : Ailitonia
-@Date           : 2022/12/01 19:53
-@FileName       : src
-@Project        : nonebot2_miya 
-@Description    : omega source
-@GitHub         : https://github.com/Ailitonia
-@Software       : PyCharm 
-"""
--- src/service/_onebot_api/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-@Author         : Ailitonia
-@Date           : 2022/03/30 20:27
-@FileName       : onebot_api.py
-@Project        : nonebot2_miya 
-@Description    : [Deactivated]Onebot Api
-@GitHub         : https://github.com/Ailitonia
-@Software       : PyCharm 
-"""
