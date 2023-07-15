3383b14dc37771a791ce0ca79ff027ba04b70487
Ailitonia <41713304+Ailitonia@users.noreply.github.com>
2023-07-15 20:32:35 +0800

    Refactor: 群自动打卡插件
    - 重构 OneBot V11 群自动打卡插件

---
+++ src/plugins/onebot_v11_auto_group_sign/config.py
@@ -0,0 +1,36 @@
+"""
+@Author         : Ailitonia
+@Date           : 2023/7/15 20:01
+@FileName       : config
+@Project        : nonebot2_miya
+@Description    : 自动群打卡配置
+@GitHub         : https://github.com/Ailitonia
+@Software       : PyCharm 
+"""
+
+from nonebot import get_driver, logger
+from pydantic import BaseModel, ValidationError
+
+
+class AutoGroupSignConfig(BaseModel):
+    """自动群打卡插件配置"""
+    # 启用自动群打卡
+    enable_auto_group_sign: bool = False
+    # 自动群打卡延迟(每日零时之后, 单位秒)
+    auto_group_sign_delay: int = 0
+
+    class Config:
+        extra = "ignore"
+
+
+try:
+    auto_group_sign_config = AutoGroupSignConfig.parse_obj(get_driver().config)
+except ValidationError as e:
+    import sys
+    logger.opt(colors=True).critical(f'<r>自动群打卡插件配置格式验证失败</r>, 错误信息:\n{e}')
+    sys.exit(f'自动群打卡插件配置格式验证失败, {e}')
+
+
+__all__ = [
+    'auto_group_sign_config'
+]
---
+++ src/plugins/onebot_v11_auto_group_sign/scheduler.py
@@ -0,0 +1,59 @@
+"""
+@Author         : Ailitonia
+@Date           : 2022/06/27 20:48
+@FileName       : auto_group_sign.py
+@Project        : nonebot2_miya 
+@Description    : 自动群打卡定时任务
+@GitHub         : https://github.com/Ailitonia
+@Software       : PyCharm 
+"""
+
+from nonebot.log import logger
+from nonebot.adapters import Bot
+
+from src.service import scheduler
+from src.service.omega_multibot_support import get_online_bots
+from src.utils.process_utils import semaphore_gather
+
+from .config import auto_group_sign_config
+
+
+async def _bot_group_sign(bot: Bot):
+    tasks = [
+        bot.send_group_sign(group_id=group_data['group_id'])
+        for group_data in await bot.get_group_list()
+        if group_data.get('group_id')
+    ]
+    await semaphore_gather(tasks=tasks, semaphore_num=16)
+
+
+async def _auto_group_sign_main() -> None:
+    logger.info('AutoGroupSign | Starting sign all groups')
+    tasks = [_bot_group_sign(bot=bot) for _, bot in get_online_bots().get('OneBot V11', {}).items()]
+    await semaphore_gather(tasks=tasks, semaphore_num=2)
+    logger.info('AutoGroupSign | Sign tasks completed')
+
+
+if auto_group_sign_config.enable_auto_group_sign:
+    scheduler.add_job(
+        _auto_group_sign_main,
+        'cron',
+        # year=None,
+        # month=None,
+        # day='*/1',
+        # week=None,
+        # day_of_week=None,
+        hour=0,
+        minute=0,
+        second=auto_group_sign_config.auto_group_sign_delay,
+        # start_date=None,
+        # end_date=None,
+        # timezone=None,
+        id='auto_group_sign',
+        coalesce=True,
+        misfire_grace_time=30
+    )
+    logger.opt(colors=True).success('<lc>AutoGroupSign</lc> | <lg>自动群打卡已启用</lg>')
+
+
+__all__ = []
