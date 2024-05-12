dfd951eb63c3dc252b4f475ad82cadd926ee3bdc
Ailitonia <41713304+Ailitonia@users.noreply.github.com>
2024-05-12 22:28:09 +0800

    Upgrade: 今天吃啥插件允许通过新增自定义附加菜单扩充菜单范围
    - 今天吃啥插件允许通过新增自定义附加菜单扩充菜单范围

--- src/plugins/what_to_eat/__init__.py
+++ src/plugins/what_to_eat/__init__.py
@@ -5,7 +5,7 @@
 @Project        : nonebot2_miya
 @Description    : 今天吃啥插件
 @GitHub         : https://github.com/Ailitonia
-@Software       : PyCharm 
+@Software       : PyCharm
 """
 
 from nonebot.plugin import PluginMetadata
