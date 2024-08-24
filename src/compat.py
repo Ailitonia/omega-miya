5fd68dda15a0a923135fbbd45cf8b7667dbc75c0
Ailitonia <41713304+Ailitonia@users.noreply.github.com>
2024-08-24 11:37:32 +0800

    Upgrade: 兼容模型解析工具

--- src/compat.py
+++ src/compat.py
@@ -8,13 +8,9 @@
 @Software       : PyCharm 
 """
 
-from typing import Annotated, Any, Type, TypeVar
+from typing import Annotated, Any
 
 from pydantic import AfterValidator, AnyUrl, AnyHttpUrl, TypeAdapter
-
-
-T = TypeVar('T')
-
 
 # Compatibility for pydantic_core._pydantic_core.Url in V2
 # See https://github.com/pydantic/pydantic/discussions/8211 and https://github.com/pydantic/pydantic/discussions/6395
@@ -24,20 +20,38 @@ AnyHttpUrlStr = Annotated[AnyHttpUrl, AfterValidator(lambda v: str(v))]
 """使用 Annotated Validator 将 AnyHttpUrl 格式转换为 str"""
 
 
-def parse_obj_as(
-        type_: Type[T],
+def parse_obj_as[T](
+        type_: type[T],
         obj: Any,
         *,
         strict: bool | None = None,
         from_attributes: bool | None = None,
         context: dict[str, Any] | None = None,
 ) -> T:
-    """`parse_obj_as` is deprecated. Use `pydantic.TypeAdapter.validate_python` instead."""
+    """Validate a Python object against the model.
+
+    `parse_obj_as` is deprecated. Use `pydantic.TypeAdapter.validate_python` instead.
+    """
     return TypeAdapter(type_).validate_python(obj, strict=strict, from_attributes=from_attributes, context=context)
+
+
+def parse_json_as[T](
+        type_: type[T],
+        data: str | bytes,
+        *,
+        strict: bool | None = None,
+        context: dict[str, Any] | None = None,
+) -> T:
+    """Validate a JSON string or bytes against the model.
+
+    Usage docs: https://docs.pydantic.dev/2.8/concepts/json/#json-parsing
+    """
+    return TypeAdapter(type_).validate_json(data, strict=strict, context=context)
 
 
 __all__ = [
     'AnyUrlStr',
     'AnyHttpUrlStr',
-    'parse_obj_as'
+    'parse_obj_as',
+    'parse_json_as',
 ]
