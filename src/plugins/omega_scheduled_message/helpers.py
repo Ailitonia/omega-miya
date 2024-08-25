0c291ee9525c5b4d816fab51be267eb4153c2707
Ailitonia <41713304+Ailitonia@users.noreply.github.com>
2024-08-25 13:34:00 +0800

    Upgrade: omega_scheduled_message 插件 (适配 OmegaMatcherInterface)

--- src/plugins/omega_scheduled_message/helpers.py
+++ src/plugins/omega_scheduled_message/helpers.py
@@ -8,16 +8,24 @@
 @Software       : PyCharm 
 """
 
+from typing import TYPE_CHECKING
+
 import ujson as json
 from apscheduler.triggers.cron import CronTrigger
-
 from nonebot import get_driver
 from nonebot.log import logger
 
 from src.database import AuthSettingDAL, begin_db_session
-from src.service import OmegaInterface, OmegaEntity, OmegaMessage, scheduler
-
+from src.service import (
+    OmegaEntityInterface as OmEI,
+    OmegaEntity,
+    OmegaMessage,
+    scheduler
+)
 from .model import SCHEDULE_MESSAGE_CUSTOM_MODULE_NAME, SCHEDULE_MESSAGE_CUSTOM_PLUGIN_NAME, ScheduleMessageJob
+
+if TYPE_CHECKING:
+    from src.service import OmegaMatcherInterface
 
 
 def add_schedule_job(job_data: ScheduleMessageJob) -> None:
@@ -29,8 +37,7 @@ def add_schedule_job(job_data: ScheduleMessageJob) -> None:
         try:
             async with begin_db_session() as session:
                 entity = await OmegaEntity.init_from_entity_index_id(session=session, index_id=job_data.entity_index_id)
-                interface = OmegaInterface(entity=entity)
-                await interface.send_msg(message=send_message)
+                await OmEI(entity=entity).send_entity_message(message=send_message)
         except Exception as e:
             logger.error(f'ScheduleMessageJob | Sending schedule message job({job_data.job_name}) failed, {e!r}')
 
@@ -61,11 +68,11 @@ def remove_schedule_job(job_data: ScheduleMessageJob) -> None:
 async def _init_schedule_message_job() -> None:
     """启动时读取并配置所有定时消息任务"""
     async with begin_db_session() as session:
-        dal = AuthSettingDAL(session=session)
-        all_jobs = await dal.query_module_plugin_all(module=SCHEDULE_MESSAGE_CUSTOM_MODULE_NAME,
-                                                     plugin=SCHEDULE_MESSAGE_CUSTOM_PLUGIN_NAME)
+        all_jobs = await AuthSettingDAL(session=session).query_module_plugin_all(
+            module=SCHEDULE_MESSAGE_CUSTOM_MODULE_NAME, plugin=SCHEDULE_MESSAGE_CUSTOM_PLUGIN_NAME
+        )
         for job in all_jobs:
-            if job.available == 1:
+            if job.available == 1 and job.value is not None:
                 try:
                     add_schedule_job(job_data=ScheduleMessageJob.model_validate(json.loads(job.value)))
                 except Exception as e:
@@ -73,7 +80,7 @@ async def _init_schedule_message_job() -> None:
 
 
 async def generate_schedule_job_data(
-        interface: OmegaInterface,
+        interface: "OmegaMatcherInterface",
         job_name: str,
         crontab: str,
         message: OmegaMessage
@@ -90,7 +97,7 @@ async def generate_schedule_job_data(
     return ScheduleMessageJob.model_validate(job_data)
 
 
-async def get_schedule_message_job_list(interface: OmegaInterface) -> list[str]:
+async def get_schedule_message_job_list(interface: "OmegaMatcherInterface") -> list[str]:
     """获取数据库中 Event 对应 Entity 的全部定时任务名称"""
     all_jobs = await interface.entity.query_plugin_all_auth_setting(
         module=SCHEDULE_MESSAGE_CUSTOM_MODULE_NAME, plugin=SCHEDULE_MESSAGE_CUSTOM_PLUGIN_NAME
@@ -98,12 +105,12 @@ async def get_schedule_message_job_list(interface: OmegaInterface) -> list[str]:
     job_list = [
         ScheduleMessageJob.model_validate(json.loads(x.value)).schedule_job_name
         for x in all_jobs
-        if x.available == 1
+        if (x.available == 1 and x.value is not None)
     ]
     return job_list
 
 
-async def set_schedule_message_job(interface: OmegaInterface, job_data: ScheduleMessageJob) -> None:
+async def set_schedule_message_job(interface: "OmegaMatcherInterface", job_data: ScheduleMessageJob) -> None:
     """在数据库中新增或更新 Event 对应 Entity 的定时任务信息"""
     await interface.entity.set_auth_setting(
         module=SCHEDULE_MESSAGE_CUSTOM_MODULE_NAME,
@@ -114,13 +121,16 @@ async def set_schedule_message_job(interface: OmegaInterface, job_data: Schedule
     )
 
 
-async def remove_schedule_message_job(interface: OmegaInterface, job_name: str) -> None:
+async def remove_schedule_message_job(interface: "OmegaMatcherInterface", job_name: str) -> None:
     """在数据库中停用 Event 对应 Entity 的定时任务信息"""
     job_setting = await interface.entity.query_auth_setting(
         module=SCHEDULE_MESSAGE_CUSTOM_MODULE_NAME,
         plugin=SCHEDULE_MESSAGE_CUSTOM_PLUGIN_NAME,
         node=job_name
     )
+    if job_setting.value is None:
+        raise ValueError(f'{interface.entity} job({job_name}) not confined')
+
     job_data = ScheduleMessageJob.model_validate(json.loads(job_setting.value))
 
     await interface.entity.set_auth_setting(
@@ -137,5 +147,5 @@ __all__ = [
     'generate_schedule_job_data',
     'get_schedule_message_job_list',
     'set_schedule_message_job',
-    'remove_schedule_message_job'
+    'remove_schedule_message_job',
 ]
