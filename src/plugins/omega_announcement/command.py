d1ea6cc999d7da57572ac654d2b7579bad044982
Ailitonia <41713304+Ailitonia@users.noreply.github.com>
2024-08-25 13:32:01 +0800

    Upgrade: omega_announcement 插件 (适配 OmegaMatcherInterface)

--- src/plugins/omega_announcement/command.py
+++ src/plugins/omega_announcement/command.py
@@ -12,7 +12,6 @@ from typing import Annotated
 
 from nonebot.adapters import Message
 from nonebot.log import logger
-from nonebot.matcher import Matcher
 from nonebot.params import Arg, Depends
 from nonebot.permission import SUPERUSER
 from nonebot.plugin import on_command
@@ -20,7 +19,12 @@ from nonebot.rule import to_me
 
 from src.database import EntityDAL
 from src.params.handler import get_command_message_arg_parser_handler
-from src.service import OmegaEntity, OmegaInterface, enable_processor_state
+from src.service import (
+    OmegaMatcherInterface as OmMI,
+    OmegaEntityInterface as OmEI,
+    OmegaEntity,
+    enable_processor_state
+)
 from src.utils.process_utils import semaphore_gather
 
 
@@ -35,31 +39,27 @@ from src.utils.process_utils import semaphore_gather
     state=enable_processor_state(name='OmegaAnnouncement', enable_processor=False)
 ).got('announcement_content', prompt='请输入公告内容:')
 async def handle_announce(
-        matcher: Matcher,
+        interface: Annotated[OmMI, Depends(OmMI.depend())],
         entity_dal: Annotated[EntityDAL, Depends(EntityDAL.dal_dependence)],
         announcement_content: Annotated[Message, Arg('announcement_content')]
 ) -> None:
-    interface = OmegaInterface()
-    announce_message = interface.get_msg_extractor()(message=announcement_content).message
+    announce_message = interface.get_message_extractor()(message=announcement_content).message
 
     all_entity = await entity_dal.query_all()
-
     tasks = []
     for entity in all_entity:
-        entity = await OmegaEntity.init_from_entity_index_id(session=entity_dal.db_session, index_id=entity.id)
-        if not await entity.check_global_permission():
+        internal_entity = await OmegaEntity.init_from_entity_index_id(session=entity_dal.db_session, index_id=entity.id)
+        if not await internal_entity.check_global_permission():
             continue
-
-        tasks.append(OmegaInterface(entity=entity).send_msg(message=announce_message))
+        tasks.append(OmEI(entity=internal_entity).send_entity_message(message=announce_message))
 
     announce_result = await semaphore_gather(tasks=tasks, semaphore_num=3)
-
     if any(isinstance(x, Exception) for x in announce_result):
         logger.warning(f'Announce | 公告批量发送完成, 部分公告发送失败')
-        await matcher.finish('公告批量发送完成, 部分对象发送失败')
+        await interface.finish_reply('公告批量发送完成, 部分对象发送失败')
     else:
         logger.success(f'Announce | 公告批量发送完成')
-        await matcher.finish('公告批量发送完成, 所有对象均已发送成功')
+        await interface.finish_reply('公告批量发送完成, 所有对象均已发送成功')
 
 
 __all__ = []
