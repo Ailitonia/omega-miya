612534b8ce5b120cdebcd60e34cba800b1c5f7c6
Ailitonia <41713304+Ailitonia@users.noreply.github.com>
2024-08-31 15:14:25 +0800

    Fix: Capitalized SQL Keyword

--- tools/migration_from_old_version/output_pixiv_as_artwork_collection(from_v092_to_v1_gamma).sql
+++ tools/migration_from_old_version/output_pixiv_as_artwork_collection(from_v092_to_v1_gamma).sql
@@ -5,30 +5,30 @@ WHERE omega_pixiv_artwork_page.page != 0;
 
 -- 导出去重后数据 --
 SELECT
-  'pixiv' as origin,
-  omega_pixiv_artwork.pid as aid,
-  omega_pixiv_artwork.title as title,
-  omega_pixiv_artwork.uid as uid,
-  omega_pixiv_artwork.uname as uname,
+  'pixiv' AS origin,
+  omega_pixiv_artwork.pid AS aid,
+  omega_pixiv_artwork.title AS title,
+  omega_pixiv_artwork.uid AS uid,
+  omega_pixiv_artwork.uname AS uname,
   CASE omega_pixiv_artwork.classified
     WHEN 0 THEN 0
     WHEN 1 THEN 3
     WHEN 2 THEN 1
     ELSE -1
-  END as classification,
+  END AS classification,
   CASE omega_pixiv_artwork.nsfw_tag
     WHEN 2 THEN 3
     WHEN 1 THEN 1
     WHEN 0 THEN 0
     ELSE -1
-  END as rating,
-  omega_pixiv_artwork.width as width,
-  omega_pixiv_artwork.height as height,
-  omega_pixiv_artwork.tags as tags,
-  NULL as description,
-  omega_pixiv_artwork.url as source,
-  omega_pixiv_artwork_page.original as cover_page,
-  omega_pixiv_artwork.created_at as created_at,
-  omega_pixiv_artwork.updated_at as updated_at
+  END AS rating,
+  omega_pixiv_artwork.width AS width,
+  omega_pixiv_artwork.height AS height,
+  omega_pixiv_artwork.tags AS tags,
+  NULL AS description,
+  omega_pixiv_artwork.url AS source,
+  omega_pixiv_artwork_page.original AS cover_page,
+  omega_pixiv_artwork.created_at AS created_at,
+  omega_pixiv_artwork.updated_at AS updated_at
 FROM omega_pixiv_artwork
 LEFT JOIN omega_pixiv_artwork_page ON omega_pixiv_artwork.id = omega_pixiv_artwork_page.artwork_index_id
