2de4a22c78be46c4c8d869ca10e02b5e42983147
Ailitonia <41713304+Ailitonia@users.noreply.github.com>
2024-09-03 02:51:30 +0800

    Fix: 变量命名重复

--- src/plugins/maybe/helpers.py
+++ src/plugins/maybe/helpers.py
@@ -25,46 +25,46 @@ def query_divination(divination_text: str, user_id: str | int) -> str:
     divination_result = random.randint(1, 108)
     # 大吉・中吉・小吉・吉・半吉・末吉・末小吉・凶・小凶・半凶・末凶・大凶
     if divination_result < 4:
-        divination_star = 0
-        divination_text = '大凶'
+        result_star = 0
+        result_text = '大凶'
     elif divination_result < 9:
-        divination_star = 1
-        divination_text = '末凶'
+        result_star = 1
+        result_text = '末凶'
     elif divination_result < 16:
-        divination_star = 2
-        divination_text = '半凶'
+        result_star = 2
+        result_text = '半凶'
     elif divination_result < 25:
-        divination_star = 3
-        divination_text = '小凶'
+        result_star = 3
+        result_text = '小凶'
     elif divination_result < 36:
-        divination_star = 4
-        divination_text = '凶'
+        result_star = 4
+        result_text = '凶'
     elif divination_result < 48:
-        divination_star = 5
-        divination_text = '末小吉'
+        result_star = 5
+        result_text = '末小吉'
     elif divination_result < 60:
-        divination_star = 6
-        divination_text = '末吉'
+        result_star = 6
+        result_text = '末吉'
     elif divination_result < 72:
-        divination_star = 7
-        divination_text = '半吉'
+        result_star = 7
+        result_text = '半吉'
     elif divination_result < 84:
-        divination_star = 8
-        divination_text = '吉'
+        result_star = 8
+        result_text = '吉'
     elif divination_result < 96:
-        divination_star = 9
-        divination_text = '小吉'
+        result_star = 9
+        result_text = '小吉'
     elif divination_result < 102:
-        divination_star = 10
-        divination_text = '中吉'
+        result_star = 10
+        result_text = '中吉'
     else:
-        divination_star = 11
-        divination_text = '大吉'
+        result_star = 11
+        result_text = '大吉'
 
     msg = (
         f'所求之事: “{divination_text}”\n\n'
-        f'结果: 【{divination_text}】\n'
-        f'{"★" * divination_star}{"☆" * (11 - divination_star)}'
+        f'结果: 【{result_text}】\n'
+        f'{"★" * result_star}{"☆" * (11 - result_star)}'
     )
 
     # 重置随机种子
