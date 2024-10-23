34f90a17689edab1294b9c66c7a149cddf896c17
Ailitonia <41713304+Ailitonia@users.noreply.github.com>
2024-10-24 03:46:31 +0800

    feat(tools): add `manual_rate_pixiv_artwork_gui` tool
    - Enhancement for #155

---
+++ tools/manual_rate_pixiv_artwork_gui/__init__.py
@@ -0,0 +1,27 @@
+"""
+@Author         : Ailitonia
+@Date           : 2024/9/8 17:05
+@FileName       : manual_rate_pixiv_artwork
+@Project        : ailitonia-toolkit
+@Description    : 
+@GitHub         : https://github.com/Ailitonia
+@Software       : PyCharm
+"""
+
+from .data_source import (
+    LocalPixivArtworkSource,
+    NonRatingPixivArtworkSource,
+    RecommendPixivArtworkSource,
+    ArtworkRecommendPixivArtworkSource,
+    SearchPopularPixivArtworkSource,
+)
+from .ui_main import ManualRatingPixivArtworkMain
+
+__all__ = [
+    'LocalPixivArtworkSource',
+    'NonRatingPixivArtworkSource',
+    'ManualRatingPixivArtworkMain',
+    'RecommendPixivArtworkSource',
+    'ArtworkRecommendPixivArtworkSource',
+    'SearchPopularPixivArtworkSource',
+]
