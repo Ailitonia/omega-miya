17780a9dc25de2d9f41255f37d031d89941eccd4
Ailitonia <41713304+ailitonia@users.noreply.github.com>
2025-02-12 10:33:43 +0800

    feat(src): `compat` 模块新增 `dump_obj_as` 方法

--- src/compat.py
+++ src/compat.py
@@ -8,7 +8,7 @@
 @Software       : PyCharm
 """
 
-from typing import Annotated, Any
+from typing import Annotated, Any, Literal
 
 from pydantic import AnyHttpUrl, AnyUrl, BeforeValidator, TypeAdapter
 
@@ -52,11 +52,49 @@ def parse_json_as[T](
     return TypeAdapter(type_).validate_json(data, strict=strict, context=context)
 
 
-def dump_json_as[T](type_: type[T], obj: Any, *, encoding: str = 'utf-8') -> str:
-    """Validate a Python object against the model and serialize it to JSON."""
+def dump_obj_as[T](
+        type_: type[T],
+        obj: Any,
+        *,
+        mode: Literal['json', 'python'] = 'python',
+        by_alias: bool = False,
+        exclude_unset: bool = False,
+        exclude_defaults: bool = False,
+        exclude_none: bool = False,
+) -> Any:
+    """Validate and dump an instance of the adapted type to a Python object."""
     custom_type_adapter = TypeAdapter(type_)
     validated_instance = custom_type_adapter.validate_python(obj)
-    return custom_type_adapter.dump_json(validated_instance).decode(encoding=encoding)
+    return custom_type_adapter.dump_python(
+        validated_instance,
+        mode=mode,
+        by_alias=by_alias,
+        exclude_unset=exclude_unset,
+        exclude_defaults=exclude_defaults,
+        exclude_none=exclude_none,
+    )
+
+
+def dump_json_as[T](
+        type_: type[T],
+        obj: Any,
+        *,
+        encoding: str = 'utf-8',
+        by_alias: bool = False,
+        exclude_unset: bool = False,
+        exclude_defaults: bool = False,
+        exclude_none: bool = False,
+) -> str:
+    """Validate and serialize an instance of the adapted type to JSON."""
+    custom_type_adapter = TypeAdapter(type_)
+    validated_instance = custom_type_adapter.validate_python(obj)
+    return custom_type_adapter.dump_json(
+        validated_instance,
+        by_alias=by_alias,
+        exclude_unset=exclude_unset,
+        exclude_defaults=exclude_defaults,
+        exclude_none=exclude_none,
+    ).decode(encoding=encoding)
 
 
 __all__ = [
@@ -64,5 +102,6 @@ __all__ = [
     'AnyHttpUrlStr',
     'parse_obj_as',
     'parse_json_as',
+    'dump_obj_as',
     'dump_json_as',
 ]
