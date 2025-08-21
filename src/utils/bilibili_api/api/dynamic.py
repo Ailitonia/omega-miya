022600eaaa0f56ab9744958fcec719e5fe17b0e7
omega-miya <208730900+omega-miya@users.noreply.github.com>
2025-08-21 17:07:53 +0800

    fix(utils): 修复 `bilibili_api` 动态获取相关方法由于未使用新版参数导致获取内容不全的问题

--- src/utils/bilibili_api/api/dynamic.py
+++ src/utils/bilibili_api/api/dynamic.py
@@ -25,12 +25,15 @@ class BilibiliDynamic(BilibiliCommon):
             host_mid: str | None = None,
             offset: int | None = None,
             update_baseline: int | None = None,
-            platform: str = 'web',
-            web_location: str = '333.1365'
     ) -> Dynamics:
         """获取我关注的动态列表更新"""
         url = 'https://api.bilibili.com/x/polymer/web-dynamic/v1/feed/all'
-        params: dict[str, str] = {'platform': platform, 'web_location': web_location}
+        params: dict[str, str] = {
+            'platform': 'web',
+            'web_location': '333.1365',
+            'features': 'itemOpusStyle,listOnlyfans,opusBigCover,onlyfansVote,'
+                        'decorationCard,onlyfansAssetsV2,ugcDelete',
+        }
         if type_ is not None:
             params.update({'type': type_})
         if host_mid is not None:
@@ -50,17 +53,17 @@ class BilibiliDynamic(BilibiliCommon):
             *,
             offset: int | None = None,
             timezone_offset: int | None = None,
-            features: str | None = None,
     ) -> Dynamics:
         """获取用户空间动态"""
         url = 'https://api.bilibili.com/x/polymer/web-dynamic/v1/feed/space'
-        params: dict[str, str] = {'host_mid': str(host_mid)}
+        params: dict[str, str] = {
+            'host_mid': str(host_mid),
+            'features': 'itemOpusStyle',
+        }
         if offset is not None:
             params.update({'offset': str(offset)})
         if timezone_offset is not None:
             params.update({'timezone_offset': str(timezone_offset)})
-        if features is not None:
-            params.update({'features': features})
 
         data = await cls._get_resource_as_json(url=url, params=params)
         return Dynamics.model_validate(data)
@@ -70,13 +73,25 @@ class BilibiliDynamic(BilibiliCommon):
             cls,
             id_: int | str,
             *,
-            timezone_offset: int | None = None,
+            timezone_offset: int = -480,
     ) -> DynDetail:
         """获取动态详细信息"""
         url = 'https://api.bilibili.com/x/polymer/web-dynamic/v1/detail'
-        params: dict[str, str] = {'id': str(id_)}
+        params: dict[str, str] = {
+            'id': str(id_),
+            'platform': 'web',
+            'gaia_source': 'main_web',
+            'features': 'itemOpusStyle,opusBigCover,onlyfansVote,endFooterHidden,decorationCard,'
+                        'onlyfansAssetsV2,ugcDelete,onlyfansQaCard,editable,opusPrivateVisible,'
+                        'avatarAutoTheme,commentsNewVersion',
+            'x-bili-device-req-json': '{"platform":"web","device":"pc"}',
+            'x-bili-web-req-json': '{"spm_id":"333.1368"}',
+        }
         if timezone_offset is not None:
             params.update({'timezone_offset': str(timezone_offset)})
+
+        # Wbi 签名 (非必要)
+        # params = await cls.sign_wbi_params(params=params)
 
         data = await cls._get_resource_as_json(url=url, params=params)
         return DynDetail.model_validate(data)
