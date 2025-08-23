769ca9484876f426cd9cc3d5a349d1855905996c
Ailitonia <41713304+ailitonia@users.noreply.github.com>
2025-08-23 14:46:11 +0800

    feat(utils)!: 移除腾讯云 API
    BREAKING CHANGE: 原使用的腾讯云 API 相关服务 (NLP) 已下线, 已使用 AI 大模型相关功能替代

--- src/utils/tencent_cloud_api/model/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-@Author         : Ailitonia
-@Date           : 2022/04/10 19:02
-@FileName       : model.py
-@Project        : nonebot2_miya
-@Description    : Tencent Cloud Model
-@GitHub         : https://github.com/Ailitonia
-@Software       : PyCharm
-"""
