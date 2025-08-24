101a9c7392ddd026a5a4a054303242f7f7349484
Ailitonia <41713304+ailitonia@users.noreply.github.com>
2025-08-24 15:01:09 +0800

    refactor(plugin): 重构 `nbnhhsh` 插件, 使用 AI 内置应用实现

--- src/plugins/nbnhhsh/config.py
+++ src/plugins/nbnhhsh/config.py
@@ -13,27 +13,33 @@ from typing import Literal
 from nonebot import get_plugin_config, logger
 from pydantic import BaseModel, ConfigDict, Field, ValidationError
 
+type JsonOutputType = Literal['json_schema', 'json_object', None]
+
 
 class NbnhhshPluginConfig(BaseModel):
     """Nbnhhsh 插件配置"""
+
     # 是否启用 AI 解释
     nbnhhsh_plugin_enable_ai_description: bool = Field(default=False)
+
     # 解释时使用的 AI 服务名称, 为 None 则使用默认配置
     nbnhhsh_plugin_ai_description_service_name: str | None = Field(default=None)
     # 解释时使用的 AI 模型名称, 为 None 则使用默认配置
     nbnhhsh_plugin_ai_description_model_name: str | None = Field(default=None)
+    # 解释时要求返回 JSON 格式, 有助于数据解析, 若 AI 服务不支持 JSON 输出则需要设置为 `None`
+    nbnhhsh_plugin_ai_description_json_output: JsonOutputType = Field(default='json_object')
+
     # 识别图片时使用的 AI 服务名称, 为 None 则使用默认配置
     nbnhhsh_plugin_ai_vision_service_name: str | None = Field(default=None)
     # 识别图片时使用的 AI 模型名称, 为 None 则使用默认配置
     nbnhhsh_plugin_ai_vision_model_name: str | None = Field(default=None)
+    # 识别图片时要求返回 JSON 格式, 有助于数据解析, 若 AI 服务不支持 JSON 输出则需要设置为 `None`
+    nbnhhsh_plugin_ai_vision_json_output: JsonOutputType = Field(default='json_object')
+
     # 生成时的 Temperature 参数值
-    nbnhhsh_plugin_ai_temperature: float = Field(default=0.4, ge=0, le=2)
+    nbnhhsh_plugin_ai_temperature: float = Field(default=0.5, ge=0, le=2)
     # 生成时的 Max Tokens 参数值
     nbnhhsh_plugin_ai_max_tokens: int = Field(default=4096)
-    # 生成时的请求超时时间
-    nbnhhsh_plugin_ai_timeout: int = Field(default=120)
-    # 要求返回 JSON 格式, 有助于数据解析, 若 AI 服务不支持 JSON 输出则需要设置为 `None`
-    nbnhhsh_plugin_ai_query_json_output: Literal['json_schema', 'json_object', None] = Field(default='json_object')
 
     model_config = ConfigDict(extra='ignore')
 
--- src/plugins/nbnhhsh/data_source.py
+++ src/plugins/nbnhhsh/data_source.py
@@ -8,49 +8,13 @@
 @Software       : PyCharm
 """
 
-from collections.abc import Iterable
-
-from lxml import etree
 from nonebot.log import logger
-from nonebot.utils import run_sync
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
 
 from src.compat import parse_obj_as
 from src.utils import OmegaRequests
-from src.utils.openai_api import ChatSession
+from src.utils.openai_api.scenario_app import ImageDescriptionApp, KnowledgeExtractorApp, WebPageDescriptionApp
 from .config import nbnhhsh_plugin_config
-from .consts import DESCRIPTION_PROMPT, IMAGE_DESC_PROMPT, WEB_DESC_PROMPT
-
-
-class ImageItems(BaseModel):
-    type: str
-    content: list[str]
-
-
-class ImageDescription(BaseModel):
-    entity: list[ImageItems]
-    image_overview: str
-
-
-class WebDescription(BaseModel):
-    keywords: list[str]
-    web_overview: str
-
-
-class ObjectDescription(BaseModel):
-    object: str
-    description: str
-
-
-class ObjectDescriptionResult(BaseModel):
-    result: list[ObjectDescription]
-
-
-class QueryContent(BaseModel):
-    image_description: ImageDescription | None = Field(default=None)
-    web_description: WebDescription | None = Field(default=None)
-    attr_description: str = Field(default_factory=str)
-    user_message: str
 
 
 class AttrGuessResult(BaseModel):
@@ -80,111 +44,14 @@ async def _query_attr_guess(guess: str) -> list[AttrGuessResult]:
     return parse_obj_as(list[AttrGuessResult], OmegaRequests.parse_content_as_json(response=response))
 
 
-async def query_attr_guess(guess: str) -> list[str]:
+async def query_abbr_guess(guess: str) -> list[str]:
     guess_result = await _query_attr_guess(guess=guess)
     return [trans_word for x in guess_result for trans_word in x.guess_result]
 
 
-@run_sync
-def get_html_pure_text(html_content: str) -> str:
-    """提取 html 中文本内容"""
-    tree = etree.HTML(html_content)
-
-    # 定义要删除的标签
-    tags_to_remove = ['script', 'style', 'meta', 'link', 'noscript']
-
-    # 遍历所有节点，删除不需要的标签
-    for tag in tags_to_remove:
-        for element in tree.xpath(f'//{tag}'):
-            element.getparent().remove(element)
-
-    # 移除换行符
-    text_list = [
-        str(x).replace('\n', '').replace('\r', '').strip()
-        for x in tree.xpath('//text()')
-    ]
-
-    return ' '.join(text.strip() for text in text_list if text.strip())
-
-
-async def query_web_page_html(url: str) -> str:
-    """获取和初步清理网页 html"""
-    page_content = OmegaRequests.parse_content_as_text(await OmegaRequests().get(url=url))
-    return await get_html_pure_text(html_content=page_content)
-
-
-async def query_web_page_description(url: str) -> WebDescription:
-    """获取网页内容描述"""
-    page_content = await query_web_page_html(url=url)
-
-    session = ChatSession.create(
-        service_name=nbnhhsh_plugin_config.nbnhhsh_plugin_ai_description_service_name,
-        model_name=nbnhhsh_plugin_config.nbnhhsh_plugin_ai_description_model_name,
-        init_system_message=WEB_DESC_PROMPT,
-    )
-
-    return await session.advance_chat(
-        page_content,
-        response_format=nbnhhsh_plugin_config.nbnhhsh_plugin_ai_query_json_output,
-        model_type=WebDescription,
-        temperature=nbnhhsh_plugin_config.nbnhhsh_plugin_ai_temperature,
-        timeout=nbnhhsh_plugin_config.nbnhhsh_plugin_ai_timeout,
-    )
-
-
-async def query_image_description(image_urls: Iterable[str]) -> ImageDescription:
-    """获取图片描述"""
-    session = ChatSession.create(
-        service_name=nbnhhsh_plugin_config.nbnhhsh_plugin_ai_vision_service_name,
-        model_name=nbnhhsh_plugin_config.nbnhhsh_plugin_ai_vision_model_name,
-    )
-
-    for image_url in image_urls:
-        await session.add_chat_image(image=image_url, encoding_web_image=True)
-
-    return await session.advance_chat(
-        IMAGE_DESC_PROMPT,
-        response_format=nbnhhsh_plugin_config.nbnhhsh_plugin_ai_query_json_output,
-        model_type=ImageDescription,
-        temperature=nbnhhsh_plugin_config.nbnhhsh_plugin_ai_temperature,
-        timeout=nbnhhsh_plugin_config.nbnhhsh_plugin_ai_timeout,
-    )
-
-
-async def query_ai_description(
-        user_message: str,
-        image_description: ImageDescription | None = None,
-        web_description: WebDescription | None = None,
-        attr_description: str = '',
-) -> list[ObjectDescription]:
-    query_content = QueryContent(
-        user_message=user_message,
-        image_description=image_description,
-        web_description=web_description,
-        attr_description=attr_description,
-    )
-
-    session = ChatSession.create(
-        service_name=nbnhhsh_plugin_config.nbnhhsh_plugin_ai_description_service_name,
-        model_name=nbnhhsh_plugin_config.nbnhhsh_plugin_ai_description_model_name,
-        init_system_message=DESCRIPTION_PROMPT,
-    )
-
-    descriptions = await session.advance_chat(
-        query_content.model_dump_json(),
-        response_format=nbnhhsh_plugin_config.nbnhhsh_plugin_ai_query_json_output,
-        model_type=ObjectDescriptionResult,
-        temperature=nbnhhsh_plugin_config.nbnhhsh_plugin_ai_temperature,
-        max_tokens=nbnhhsh_plugin_config.nbnhhsh_plugin_ai_max_tokens,
-        timeout=nbnhhsh_plugin_config.nbnhhsh_plugin_ai_timeout,
-    )
-
-    return descriptions.result
-
-
 async def simple_guess(query_message: str) -> str:
     """查询缩写"""
-    guess_result = await query_attr_guess(guess=query_message)
+    guess_result = await query_abbr_guess(guess=query_message)
     if guess_result:
         trans = '\n'.join(guess_result)
         trans = f'为你找到了{query_message!r}的以下解释:\n\n{trans}'
@@ -193,56 +60,77 @@ async def simple_guess(query_message: str) -> str:
     return trans
 
 
-async def ai_guess(query_message: str, msg_images: Iterable[str]) -> str:
+async def ai_guess(query_message: str, msg_image: str | None = None) -> str:
     """使用 AI 进行解释"""
 
     # 只有文本内容为纯字母的时候才尝试查询缩写
     need_query_attr = query_message.isalpha() and query_message.isascii()
 
     try:
-        if msg_images:
-            images_desc = await query_image_description(image_urls=msg_images)
+        if msg_image:
+            images_desc = await ImageDescriptionApp(
+                service_name=nbnhhsh_plugin_config.nbnhhsh_plugin_ai_vision_service_name,
+                model_name=nbnhhsh_plugin_config.nbnhhsh_plugin_ai_vision_model_name,
+            ).describe_image(
+                image_url=msg_image,
+                response_format=nbnhhsh_plugin_config.nbnhhsh_plugin_ai_vision_json_output,
+                temperature=nbnhhsh_plugin_config.nbnhhsh_plugin_ai_temperature,
+            )
             need_query_attr = False
+            input_keywords = [x.model_dump() for x in images_desc.entity]
+            image_overview = images_desc.image_overview
         else:
-            images_desc = None
+            input_keywords = []
+            image_overview = ''
     except Exception as e:
-        logger.warning(f'nbnhhsh | 尝试解析图片({msg_images})失败, {e}')
-        images_desc = None
+        logger.warning(f'nbnhhsh | 尝试解析图片({msg_image})失败, {e}')
+        input_keywords = []
+        image_overview = ''
 
     try:
         if urls := OmegaRequests.get_url_in_text(text=query_message):
-            web_desc = await query_web_page_description(url=urls[0])
+            web_desc = await WebPageDescriptionApp(
+                service_name=nbnhhsh_plugin_config.nbnhhsh_plugin_ai_description_service_name,
+                model_name=nbnhhsh_plugin_config.nbnhhsh_plugin_ai_description_model_name,
+            ).describe_web_page(
+                page_url=urls[0],
+                response_format=nbnhhsh_plugin_config.nbnhhsh_plugin_ai_description_json_output,
+                temperature=nbnhhsh_plugin_config.nbnhhsh_plugin_ai_temperature,
+            )
             need_query_attr = False
+            input_keywords.append({'type': 'web_page_keywords', 'content': web_desc.keywords})
+            web_overview = web_desc.web_overview
         else:
-            web_desc = None
+            web_overview = ''
     except Exception as e:
         logger.warning(f'nbnhhsh | 尝试解析链接({query_message})失败, {e}')
-        web_desc = None
+        web_overview = ''
 
     try:
-        if need_query_attr and (attr_desc_result := await query_attr_guess(guess=query_message)):
-            attr_desc = f'查询缩写{query_message!r}可能的含义:\n\n{"\n".join(attr_desc_result)}'
+        if need_query_attr and (attr_desc_result := await query_abbr_guess(guess=query_message)):
+            input_keywords.append({'type': 'abbreviation_explanation', 'content': attr_desc_result})
+            abbr_overview = f'查询缩写{query_message!r}可能的含义:\n\n{"\n".join(attr_desc_result)}'
         else:
-            attr_desc = ''
+            abbr_overview = ''
     except Exception as e:
         logger.warning(f'nbnhhsh | 查询{query_message!r}缩写失败, {e}')
-        attr_desc = ''
+        abbr_overview = ''
 
-    desc_result = await query_ai_description(
+    knowledge_extract_result = await KnowledgeExtractorApp(
+        service_name=nbnhhsh_plugin_config.nbnhhsh_plugin_ai_description_service_name,
+        model_name=nbnhhsh_plugin_config.nbnhhsh_plugin_ai_description_model_name,
+    ).extract_knowledge(
         user_message=query_message,
-        image_description=images_desc,
-        web_description=web_desc,
-        attr_description=attr_desc,
+        input_keywords=input_keywords,
+        response_format=nbnhhsh_plugin_config.nbnhhsh_plugin_ai_description_json_output,
+        temperature=nbnhhsh_plugin_config.nbnhhsh_plugin_ai_temperature,
+        max_tokens=nbnhhsh_plugin_config.nbnhhsh_plugin_ai_max_tokens,
     )
 
-    pre_message = (
-            attr_desc
-            or (images_desc.image_overview if images_desc else '')
-            or (web_desc.web_overview if web_desc else '')
-    )
+    pre_message = f'{abbr_overview}\n\n{image_overview}\n\n{web_overview}'
 
-    if desc_result:
-        desc_text = '\n\n'.join(f'{x.object}: {x.description}' for x in desc_result)
+    if knowledge_extract_result.result:
+        desc_text = '\n\n'.join(f'{x.object}: {x.description}' for x in knowledge_extract_result.result)
     else:
         desc_text = '没有识别到相关需要解释的实体或概念'
 
