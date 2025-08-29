f36adb812c2dc3484c81b26bf70c90f1dbe08c8d
Ailitonia <41713304+ailitonia@users.noreply.github.com>
2025-08-29 15:08:17 +0800

    feat(plugin)!: 模仿写作插件改为使用内置模板生成器

--- src/plugins/imitating_writing/command.py
+++ src/plugins/imitating_writing/command.py
@@ -13,21 +13,21 @@ from typing import Annotated
 from nonebot.log import logger
 from nonebot.params import ArgStr
 from nonebot.plugin import on_command
+from nonebot.typing import T_State
 
 from src.params.depends import EVENT_MATCHER_INTERFACE
 from src.params.handler import get_command_str_multi_args_parser_handler
 from src.service import enable_processor_state
-from src.utils.openai_api.scenario_app.imitating_writing import (
-    BaseImitatingWritingApp,
-    CustomImitatingWritingApp,
-    ProgrammingPhilosophyImitatingWritingApp,
-)
+from src.utils.openai_api.scenario_app import CustomImitatingWritingApp, TemplateImitatingWritingAppGenerator
 from .config import imitating_writing_plugin_config
 
-_APP_MAP: dict[str, type[BaseImitatingWritingApp]] = {
-    '编程哲学': ProgrammingPhilosophyImitatingWritingApp,
+_APP_MAP: dict[str, str] = {
+    '编程哲学': 'programming_philosophy.md',
+    '四大名著': 'the_four_great_classical_novels.md',
+    '你说得对': 'you_are_right_but.md',
 }
-"""内置模仿写作应用"""
+"""内置模仿写作应用名称与模板文件名映射"""
+
 
 imitating_writing = on_command(
     'imitating-writing',
@@ -47,14 +47,19 @@ async def handle_imitating_writing(
         interface: EVENT_MATCHER_INTERFACE,
         template: Annotated[str | None, ArgStr('writing_arg_0')],
         keyword: Annotated[str | None, ArgStr('writing_arg_1')],
+        state: T_State,
 ) -> None:
+    reply_message = interface.get_event_reply_msg_plain_text()
+    template = template or reply_message or state.get('writing_arg_0', None)
+    state['writing_arg_0'] = template
+
     if template is None:
         await interface.reject_arg_reply('writing_arg_0', '请输入你想要模仿写作的内容模板:')
 
     template = template.strip()
-    if len(template) <= 16 and template not in _APP_MAP:
+    if len(template) < 14 and template not in _APP_MAP:
         template_name_text = f'{template!r}不是可用的内置模板, 当前可用模板有:\n\n{"\n".join(x for x in _APP_MAP.keys())}'
-        await interface.reject_arg_reply('writing_arg_0', f'{template_name_text}\n请输入你想要使用的模板:')
+        await interface.reject_arg_reply('writing_arg_0', f'{template_name_text}\n\n请输入你想要使用的模板:')
 
     if keyword is None:
         await interface.reject_arg_reply('writing_arg_1', '请输入你想要模仿写作的关键词:')
@@ -65,28 +70,28 @@ async def handle_imitating_writing(
 
     try:
         if template in _APP_MAP:
-            app = _APP_MAP[template](
+            app_type = TemplateImitatingWritingAppGenerator.generate_internal_imitating_writing_app(
+                template_file_name=_APP_MAP[template]
+            )
+            result = await app_type(
                 service_name=imitating_writing_plugin_config.imitating_writing_plugin_ai_service_name,
                 model_name=imitating_writing_plugin_config.imitating_writing_plugin_ai_model_name,
-
-            )
-            result = await app.write(
+            ).write(
                 text=keyword,
                 temperature=imitating_writing_plugin_config.imitating_writing_plugin_ai_temperature,
                 max_tokens=imitating_writing_plugin_config.imitating_writing_plugin_ai_max_tokens,
             )
         else:
-            app = CustomImitatingWritingApp(
+            result = await CustomImitatingWritingApp(
                 service_name=imitating_writing_plugin_config.imitating_writing_plugin_ai_service_name,
                 model_name=imitating_writing_plugin_config.imitating_writing_plugin_ai_model_name,
-            )
-            result = await app.custom_write(
+            ).custom_write(
                 template=template,
                 keyword=keyword,
                 temperature=imitating_writing_plugin_config.imitating_writing_plugin_ai_temperature,
                 max_tokens=imitating_writing_plugin_config.imitating_writing_plugin_ai_max_tokens,
             )
-        await interface.send_reply(result)
+        await interface.send_reply(result.strip().removeprefix('```').removesuffix('```').strip())
     except Exception as e:
         logger.error(f'ImitatingWriting | 写作失败, {e}')
         await interface.send_reply('模仿写作失败了QAQ, 发生了意外的错误, 请稍后再试')
