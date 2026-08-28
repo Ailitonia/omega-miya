8f44f2e72ced649e99d4fd27069a648913df2b71
Ailitonia <41713304+ailitonia@users.noreply.github.com>
2026-08-28 13:53:59 +0800

    feat(database)!: 修改数据库 DAL 基类及数据导出模型基类

--- src/database/model.py
+++ src/database/model.py
@@ -9,7 +9,6 @@
 """
 
 import abc
-import warnings
 from collections.abc import AsyncGenerator
 from contextlib import asynccontextmanager
 from typing import TYPE_CHECKING, Self
@@ -20,86 +19,87 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from .helpers import database_session
 
 if TYPE_CHECKING:
-    from .schema_base import OmegaDeclarativeBase
+    from .schema_base import OmegaDeclarativeBase as Base
 
 
-class BaseDataQueryResultModel(BaseModel):
-    """数据库查询结果数据模型基类"""
+class BaseDataOutModel(BaseModel):
+    """数据库导出数据模型基类"""
 
     model_config = ConfigDict(extra='ignore', coerce_numbers_to_str=True, from_attributes=True, frozen=True)
 
 
-class BaseDataAccessLayerModel[TB: 'OmegaDeclarativeBase', TR: BaseDataQueryResultModel](abc.ABC):
-    """数据库操作对象基类"""
+class BaseDataAccessLayer[ORM_T: 'Base', DATA_T: BaseDataOutModel](abc.ABC):
+    """数据库操作对象 (DAL) 基类
+
+    Note: 为保证兼容性, 只使用 ORM 层相关方法, 对于 add_ignore_exist 或 add_update_exist 等操作不使用
+    on_duplicate_key_update()/on_conflict_do_nothing()/on_conflict_do_update() 等方言, 对于常见竞态
+    使用唯一约束+捕获冲突异常或加锁处理, 可能会导致并发性能降低;
+    为约束事务范围, ORM 实例对象应当仅在 DAL 层内部传递, 返回给外部的应当为 DataOutModel 数据模型
+    """
 
     def __init__(self, session: AsyncSession):
         self.db_session = session
 
     @classmethod
     @asynccontextmanager
-    async def begin_dal_session(cls) -> AsyncGenerator[Self, None]:
-        """初始化 DAL 并开始会话"""
+    async def create(cls) -> AsyncGenerator[Self, None]:
+        """创建数据库 session, 初始化 DAL 并开始会话"""
         async with database_session() as session:
             yield cls(session)
 
     @classmethod
     async def dal_dependence(cls) -> AsyncGenerator[Self, None]:
         """获取 DAL 生成器依赖 (Dependence for database async session)"""
-        async with cls.begin_dal_session() as dal:
+        async with cls.create() as dal:
             yield dal
 
-    async def _add(self, obj: TB) -> None:
-        """内部方法, 向数据库插入新行"""
-        self.db_session.add(obj)
-        await self.db_session.flush()
+    @asynccontextmanager
+    async def safe_begin_transaction(self) -> AsyncGenerator[AsyncSession, None]:
+        """安全开启一个事务或嵌套事务
 
-    async def _merge(self, obj: TB) -> None:
-        """内部方法, 向数据库插入新行, 如主键存在则更新"""
-        await self.db_session.merge(obj)
-        await self.db_session.flush()
+        若当前 session 已有事务则开 SAVEPOINT, 无事务则开顶层事务
+        """
+        if not self.db_session.is_active:
+            raise RuntimeError('Current session is not active')
+
+        if self.db_session.in_transaction():
+            async with self.db_session.begin_nested():
+                yield self.db_session
+        else:
+            async with self.db_session.begin():
+                yield self.db_session
+
+    @asynccontextmanager
+    async def must_begin_nested_in_transaction(self) -> AsyncGenerator[AsyncSession, None]:
+        """开启嵌套事务
+
+        若当前 session 已有事务则开 SAVEPOINT, 无事务则抛出异常
+        """
+        if not self.db_session.is_active:
+            raise RuntimeError('Current session is not active')
+
+        if not self.db_session.in_transaction():
+            raise RuntimeError('Must be executed within a session already in active transaction')
+
+        async with self.db_session.begin_nested():
+            yield self.db_session
 
     @abc.abstractmethod
-    async def query_unique(self, *args, **kwargs) -> TR:
-        """查询唯一行"""
+    async def _select_unique(self, *args, **kwargs) -> ORM_T:
+        """内部方法, 根据非索引的条件查询唯一行, 使用 scalar_one() 获取实例对象, 若查询结果为空或不唯一则抛出异常"""
         raise NotImplementedError
 
     @abc.abstractmethod
-    async def query_all(self) -> list[TR]:
-        """查询全部行"""
-        raise NotImplementedError
-
-    @abc.abstractmethod
-    async def add(self, *args, **kwargs) -> None:
-        """新增行"""
-        raise NotImplementedError
-
-    @abc.abstractmethod
-    async def upsert(self, *args, **kwargs) -> None:
-        """新增行, 若主键存在则更新行"""
-        raise NotImplementedError
-
-    @abc.abstractmethod
-    async def update(self, *args, **kwargs) -> None:
-        """更新行"""
-        raise NotImplementedError
-
-    @abc.abstractmethod
-    async def delete(self, *args, **kwargs) -> None:
-        """删除行"""
+    async def query_unique(self, *args, **kwargs) -> DATA_T:
+        """根据非索引的条件查询唯一行, 若查询结果为空或不唯一则抛出异常"""
         raise NotImplementedError
 
     async def commit_session(self) -> None:
-        """强制提交所有数据库更改并结束 session"""
-        warnings.warn(
-            'commit_session() 已弃用, 数据库子依赖已使用上下文管理器控制事务流程, 无需手动关闭会话',
-            DeprecationWarning,
-            stacklevel=2
-        )
+        """提交所有数据库更改"""
         await self.db_session.commit()
-        await self.db_session.close()
 
 
 __all__ = [
-    'BaseDataAccessLayerModel',
-    'BaseDataQueryResultModel',
+    'BaseDataAccessLayer',
+    'BaseDataOutModel',
 ]
