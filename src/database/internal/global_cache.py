9c364183b455757a2928e4598426cc1480fe9482
Ailitonia <41713304+ailitonia@users.noreply.github.com>
2026-08-28 18:32:41 +0800

    feat(database)!: Upgrade GlobalCacheDAL

--- src/database/internal/global_cache.py
+++ src/database/internal/global_cache.py
@@ -10,15 +10,16 @@
 
 from datetime import datetime, timedelta
 
-from sqlalchemy import delete, select
+from sqlalchemy import delete, func, select
+from sqlalchemy.exc import IntegrityError
 
 from src.compat import parse_obj_as
-from ..model import BaseDataAccessLayerModel, BaseDataQueryResultModel
+from ..model import BaseDataAccessLayer, BaseDataOutModel
 from ..schema import GlobalCacheOrm
 
 
-class GlobalCache(BaseDataQueryResultModel):
-    """全局缓存 Model"""
+class GlobalCache(BaseDataOutModel):
+    """全局缓存数据"""
     cache_name: str
     cache_key: str
     cache_value: str
@@ -27,10 +28,29 @@ class GlobalCache(BaseDataQueryResultModel):
     updated_at: datetime | None
 
 
-class GlobalCacheDAL(BaseDataAccessLayerModel[GlobalCacheOrm, GlobalCache]):
-    """全局缓存 数据库操作对象"""
+class GlobalCacheDAL(BaseDataAccessLayer[GlobalCacheOrm, GlobalCache]):
+    """全局缓存"""
 
-    async def query_unique(self, cache_name: str, cache_key: str, *, include_expired: bool = False) -> GlobalCache:
+    async def _count_all(self) -> int:
+        """查询全表行数"""
+        stmt = select(func.count()).select_from(GlobalCacheOrm)
+        return (await self.db_session.execute(stmt)).scalar_one()
+
+    async def _clear_all(self) -> None:
+        """清空全表, 敏感操作, 方法内不执行 commit, 可由外层事务 rollback"""
+        await self.db_session.execute(delete(GlobalCacheOrm))
+        self.db_session.expunge_all()
+
+    async def _select_unique(
+            self,
+            cache_name: str,
+            cache_key: str,
+            *,
+            include_expired: bool = False,
+            populate_existing: bool = False,
+            with_for_update: bool = False,
+            nowait_for_update: bool = False,
+    ) -> GlobalCacheOrm:
         stmt = (select(GlobalCacheOrm)
                 .where(GlobalCacheOrm.cache_name == cache_name)
                 .where(GlobalCacheOrm.cache_key == cache_key))
@@ -38,26 +58,46 @@ class GlobalCacheDAL(BaseDataAccessLayerModel[GlobalCacheOrm, GlobalCache]):
         if not include_expired:
             stmt = stmt.where(GlobalCacheOrm.expired_at >= datetime.now())
 
-        session_result = await self.db_session.execute(stmt)
-        return GlobalCache.model_validate(session_result.scalar_one())
+        if populate_existing:
+            stmt = stmt.execution_options(populate_existing=True)
 
-    async def query_series(self, cache_name: str, *, include_expired: bool = False) -> list[GlobalCache]:
+        if with_for_update:
+            stmt = stmt.with_for_update(nowait=nowait_for_update)
+
+        return (await self.db_session.execute(stmt)).scalar_one()
+
+    async def query_unique(
+            self,
+            cache_name: str,
+            cache_key: str,
+            *,
+            include_expired: bool = False,
+            populate_existing: bool = False,
+    ) -> GlobalCache:
+        item = await self._select_unique(
+            cache_name,
+            cache_key,
+            include_expired=include_expired,
+            populate_existing=populate_existing,
+        )
+        return GlobalCache.model_validate(item)
+
+    async def query_series(
+            self,
+            cache_name: str,
+            *,
+            include_expired: bool = False,
+            populate_existing: bool = False,
+    ) -> list[GlobalCache]:
         stmt = select(GlobalCacheOrm).where(GlobalCacheOrm.cache_name == cache_name)
 
         if not include_expired:
             stmt = stmt.where(GlobalCacheOrm.expired_at >= datetime.now())
 
-        session_result = await self.db_session.execute(stmt)
-        return parse_obj_as(list[GlobalCache], session_result.scalars().all())
+        if populate_existing:
+            stmt = stmt.execution_options(populate_existing=True)
 
-    async def query_all(self, *, include_expired: bool = False) -> list[GlobalCache]:
-        stmt = select(GlobalCacheOrm).order_by(GlobalCacheOrm.cache_name)
-
-        if not include_expired:
-            stmt = stmt.where(GlobalCacheOrm.expired_at >= datetime.now())
-
-        session_result = await self.db_session.execute(stmt)
-        return parse_obj_as(list[GlobalCache], session_result.scalars().all())
+        return parse_obj_as(list[GlobalCache], (await self.db_session.execute(stmt)).scalars().all())
 
     async def add(
             self,
@@ -65,42 +105,75 @@ class GlobalCacheDAL(BaseDataAccessLayerModel[GlobalCacheOrm, GlobalCache]):
             cache_key: str,
             cache_value: str,
             expired_time: datetime | timedelta | None = None,
-    ) -> None:
+    ) -> GlobalCache:
+        """向数据库插入新行, 不校验唯一性
+
+        尽量保证插入的是新数据时才使用, 冲突直接抛出异常
+        """
         if expired_time is None:
             expired_at = datetime(year=9999, month=12, day=31)
         elif isinstance(expired_time, datetime):
             expired_at = expired_time
         else:
             expired_at = datetime.now() + expired_time
-        new_obj = GlobalCacheOrm(cache_name=cache_name, cache_key=cache_key,
-                                 cache_value=cache_value, expired_at=expired_at, created_at=datetime.now())
-        await self._add(new_obj)
+        new_obj = GlobalCacheOrm(
+            cache_name=cache_name,
+            cache_key=cache_key,
+            cache_value=cache_value,
+            expired_at=expired_at,
+        )
+        self.db_session.add(new_obj)
+        await self.db_session.flush()
+        await self.db_session.refresh(new_obj)
+        return GlobalCache.model_validate(new_obj)
 
-    async def upsert(
+    async def add_update_exist(
             self,
             cache_name: str,
             cache_key: str,
             cache_value: str,
             expired_time: datetime | timedelta | None = None,
-    ) -> None:
+    ) -> GlobalCache:
+        """向数据库插入新行, 存在则更新
+
+        通过捕获异常实现, 性能较差, 且并发时仍可能出现死锁或需要重试
+        """
         if expired_time is None:
             expired_at = datetime(year=9999, month=12, day=31)
         elif isinstance(expired_time, datetime):
             expired_at = expired_time
         else:
             expired_at = datetime.now() + expired_time
-        new_obj = GlobalCacheOrm(cache_name=cache_name, cache_key=cache_key,
-                                 cache_value=cache_value, expired_at=expired_at, updated_at=datetime.now())
-        await self._merge(new_obj)
 
-    async def update(self, *args, **kwargs) -> None:
-        raise NotImplementedError
+        new_obj = GlobalCacheOrm(
+            cache_name=cache_name,
+            cache_key=cache_key,
+            cache_value=cache_value,
+            expired_at=expired_at,
+        )
 
-    async def delete(self, cache_name: str, cache_key: str) -> None:
-        stmt = (delete(GlobalCacheOrm)
-                .where(GlobalCacheOrm.cache_name == cache_name)
-                .where(GlobalCacheOrm.cache_key == cache_key))
-        await self.db_session.execute(stmt)
+        try:
+            async with self.safe_begin_transaction() as session:
+                session.add(new_obj)
+                await session.flush()
+            await session.refresh(new_obj)
+            return GlobalCache.model_validate(new_obj)
+        except IntegrityError:
+            if new_obj in self.db_session:
+                self.db_session.expunge(new_obj)
+            async with self.safe_begin_transaction() as session:
+                exist_obj = await self._select_unique(
+                    cache_name,
+                    cache_key,
+                    include_expired=True,
+                    populate_existing=True,
+                    with_for_update=True,
+                )
+                exist_obj.cache_value = cache_value
+                exist_obj.expired_at = expired_at
+                await session.flush()
+            await session.refresh(exist_obj)
+            return GlobalCache.model_validate(exist_obj)
 
     async def delete_series_expired(self, cache_name: str) -> None:
         stmt = (delete(GlobalCacheOrm)
